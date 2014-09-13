f87f3c5fa668917c74b24991c37043ea25372f72
Ian Munsie <darkstarsword@gmail.com>
2014-09-14 02:20:14 +1000

    mkrelease.sh: Rename README.md to 3Dfix-README.txt in the zip file

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,6 +5,10 @@
 
 dir=$(readlink -f "$1")
 game=$(basename "$dir")
+readme_src=README.md
+readme_dst=3Dfix-README.txt
+tmp_dir=$(dirname $(readlink -f "$0"))/__MKRELEASE_TMP__
+rm_tmp_dir=
 
 if [ ! -d "$dir" ]; then
 	echo "Usage: $0 game"
@@ -14,4 +18,18 @@ fi
 zip=${PWD}/3Dfix-${game}-$(date '+%Y-%m-%d').zip
 
 rm -fv "$zip" || true
+
+if [ -f "$dir/$readme_src" ]; then
+	rm -frv "$tmp_dir"
+	cp -as "$dir" "$tmp_dir"
+	mv "$tmp_dir/$readme_src" "$tmp_dir/$readme_dst"
+	dir="$tmp_dir"
+	rm_tmp_dir=1
+
+fi
+
 cd "$dir" && zip -9 -r "$zip" .
+
+if [ "$rm_tmp_dir" = 1 ]; then
+	rm -frv "$tmp_dir"
+fi
