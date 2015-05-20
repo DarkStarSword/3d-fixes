4f3d659d42285701a4ae15859656e4b6b4ecc2dd
Ian Munsie <darkstarsword@gmail.com>
2015-05-20 13:43:13 +1000

    mkrelease: Automatically add an annotated git tag for each release
    This should help me keep track of which point in history was responsible
    for each release.

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,18 +4,20 @@
 # symbolically linked DLLs are correctly resolved in the zip file.
 
 dir=$(readlink -f "$1")
+tag="$2"
 game=$(basename "$dir")
 readme_src=README.md
 readme_dst=3Dfix-README.txt
 tmp_dir=$(dirname $(readlink -f "$0"))/__MKRELEASE_TMP__
 rm_tmp_dir=
+date=$(date '+%Y-%m-%d')
 
-if [ ! -d "$dir" ]; then
-	echo "Usage: $0 game"
+if [ ! -d "$dir" -o ! "$tag" ]; then
+	echo "Usage: $0 game tag"
 	exit 1
 fi
 
-zip=${PWD}/3Dfix-${game}-$(date '+%Y-%m-%d').zip
+zip=${PWD}/3Dfix-${game}-$date.zip
 
 rm -fv "$zip" || true
 
@@ -30,14 +32,18 @@ if [ -f "$dir/$readme_src" ]; then
 fi
 
 if [ -d "$dir/zip" ]; then
-	cd "$dir/zip" && zip -9 -r "$zip" . --exclude \*.swp
+	pushd "$dir/zip" && zip -9 -r "$zip" . --exclude \*.swp
 	if [ -f "$dir/$readme_dst" ]; then
 		zip -9 -j "$zip" "$dir/$readme_dst"
 	fi
 else
-	cd "$dir" && zip -9 -r "$zip" . --exclude \*.swp
+	pushd "$dir" && zip -9 -r "$zip" . --exclude \*.swp
 fi
+
+popd
 
 if [ "$rm_tmp_dir" = 1 ]; then
 	rm -frv "$tmp_dir"
 fi
+
+git tag -f "$tag-$date" -m "$game $date"
