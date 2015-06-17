4fc71cab61c0cbcc147ba551e3854b8e3784938a
Ian Munsie <darkstarsword@gmail.com>
2015-06-17 19:01:53 +1000

    ddsinfo: Add support for several more formats

--- ddsinfo.py
+++ ddsinfo.py
@@ -35,11 +35,16 @@ def scale8bit(buf, bytes):
 
 def scale_float(buf):
 	# TODO: Make clipping & scaling configurable (e.g. for HDR rendering like Witcher 3)
-	return np.clip(gamma(buf) * 255.0, 0, 255.0)
+	return np.uint8(np.clip(gamma(buf) * 255.0, 0, 255.0))
 
 def convert_R24G8_UINT(buf):
 	r = scale8bit(buf, 3)
 	g = buf >> 24
+	return np.uint8(np.column_stack((r, g, [0]*len(r))))
+
+def convert_2x16f(buf):
+	r = scale_float(buf['f0'])
+	g = scale_float(buf['f1'])
 	return np.uint8(np.column_stack((r, g, [0]*len(r))))
 
 def convert_4x16f(buf):
@@ -50,11 +55,97 @@ def convert_4x16f(buf):
 	return np.uint8(np.column_stack((r, g, b, a)))
 
 d3d9_pixel_formats = {
+	# https://msdn.microsoft.com/en-us/library/windows/desktop/bb172558(v=vs.85).aspx
+	111: ('D3DFMT_R16F', np.float16, 'L', scale_float),
+	112: ('D3DFMT_G16R16F', np.dtype('<f2, <f2'), 'RGB', convert_2x16f),
 	113: ('D3DFMT_A16B16G16R16F', np.dtype('<f2, <f2, <f2, <f2'), 'RGBA', convert_4x16f),
+	114: ('D3DFMT_R32F', np.float32, 'L', scale_float),
+
+#    D3DFMT_R8G8B8               = 20,
+#    D3DFMT_A8R8G8B8             = 21,
+#    D3DFMT_X8R8G8B8             = 22,
+#    D3DFMT_R5G6B5               = 23,
+#    D3DFMT_X1R5G5B5             = 24,
+#    D3DFMT_A1R5G5B5             = 25,
+#    D3DFMT_A4R4G4B4             = 26,
+#    D3DFMT_R3G3B2               = 27,
+#    D3DFMT_A8                   = 28,
+#    D3DFMT_A8R3G3B2             = 29,
+#    D3DFMT_X4R4G4B4             = 30,
+#    D3DFMT_A2B10G10R10          = 31,
+#    D3DFMT_A8B8G8R8             = 32,
+#    D3DFMT_X8B8G8R8             = 33,
+#    D3DFMT_G16R16               = 34,
+#    D3DFMT_A2R10G10B10          = 35,
+#    D3DFMT_A16B16G16R16         = 36,
+#
+#    D3DFMT_A8P8                 = 40,
+#    D3DFMT_P8                   = 41,
+#
+#    D3DFMT_L8                   = 50,
+#    D3DFMT_A8L8                 = 51,
+#    D3DFMT_A4L4                 = 52,
+#
+#    D3DFMT_V8U8                 = 60,
+#    D3DFMT_L6V5U5               = 61,
+#    D3DFMT_X8L8V8U8             = 62,
+#    D3DFMT_Q8W8V8U8             = 63,
+#    D3DFMT_V16U16               = 64,
+#    D3DFMT_A2W10V10U10          = 67,
+#
+#    D3DFMT_UYVY                 = MAKEFOURCC('U', 'Y', 'V', 'Y'),
+#    D3DFMT_R8G8_B8G8            = MAKEFOURCC('R', 'G', 'B', 'G'),
+#    D3DFMT_YUY2                 = MAKEFOURCC('Y', 'U', 'Y', '2'),
+#    D3DFMT_G8R8_G8B8            = MAKEFOURCC('G', 'R', 'G', 'B'),
+#    D3DFMT_DXT1                 = MAKEFOURCC('D', 'X', 'T', '1'),
+#    D3DFMT_DXT2                 = MAKEFOURCC('D', 'X', 'T', '2'),
+#    D3DFMT_DXT3                 = MAKEFOURCC('D', 'X', 'T', '3'),
+#    D3DFMT_DXT4                 = MAKEFOURCC('D', 'X', 'T', '4'),
+#    D3DFMT_DXT5                 = MAKEFOURCC('D', 'X', 'T', '5'),
+#
+#    D3DFMT_D16_LOCKABLE         = 70,
+#    D3DFMT_D32                  = 71,
+#    D3DFMT_D15S1                = 73,
+#    D3DFMT_D24S8                = 75,
+#    D3DFMT_D24X8                = 77,
+#    D3DFMT_D24X4S4              = 79,
+#    D3DFMT_D16                  = 80,
+#
+#    D3DFMT_D32F_LOCKABLE        = 82,
+#    D3DFMT_D24FS8               = 83,
+#
+##if !defined(D3D_DISABLE_9EX)
+#    D3DFMT_D32_LOCKABLE         = 84,
+#    D3DFMT_S8_LOCKABLE          = 85,
+##endif // !D3D_DISABLE_9EX
+#
+#    D3DFMT_L16                  = 81,
+#
+#    D3DFMT_VERTEXDATA           =100,
+#    D3DFMT_INDEX16              =101,
+#    D3DFMT_INDEX32              =102,
+#
+#    D3DFMT_Q16W16V16U16         =110,
+#
+#    D3DFMT_MULTI2_ARGB8         = MAKEFOURCC('M','E','T','1'),
+#
+#    D3DFMT_G32R32F              = 115,
+#    D3DFMT_A32B32G32R32F        = 116,
+#
+#    D3DFMT_CxV8U8               = 117,
+#
+##if !defined(D3D_DISABLE_9EX)
+#    D3DFMT_A1                   = 118,
+#    D3DFMT_A2B10G10R10_XR_BIAS  = 119,
+#    D3DFMT_BINARYBUFFER         = 199,
+##endif // !D3D_DISABLE_9EX
+#
+#    D3DFMT_FORCE_DWORD          =0x7fffffff
 }
 
 dxgi_formats = {
 	# https://msdn.microsoft.com/en-us/library/windows/desktop/bb173059(v=vs.85).aspx
+	# 26: ('DXGI_FORMAT_R11G11B10_FLOAT', np.dtype('u4'), 'RGB')
 	44: ('DXGI_FORMAT_R24G8_TYPELESS', np.dtype('<u4'), 'RGB', convert_R24G8_UINT),
 
 #   DXGI_FORMAT_R32G32B32A32_TYPELESS       = 1,
@@ -82,7 +173,6 @@ dxgi_formats = {
 #   DXGI_FORMAT_R10G10B10A2_TYPELESS        = 23,
 #   DXGI_FORMAT_R10G10B10A2_UNORM           = 24,
 #   DXGI_FORMAT_R10G10B10A2_UINT            = 25,
-#   DXGI_FORMAT_R11G11B10_FLOAT             = 26,
 #   DXGI_FORMAT_R8G8B8A8_TYPELESS           = 27,
 #   DXGI_FORMAT_R8G8B8A8_UNORM              = 28,
 #   DXGI_FORMAT_R8G8B8A8_UNORM_SRGB         = 29,
@@ -297,6 +387,8 @@ class DDSHeaderDXT10(object):
 		ret.append('Array Size: %i' % self.array_size)
 		ret.append('Misc Flags 1: 0x%x' % self.misc_flags)
 		ret.append('Misc Flags 2: 0x%x' % self.misc_flags2)
+		assert(self.dimension == 3) # If this fails I'll need support for Texture1D and/or Texture3D files
+		assert(self.array_size == 1) # If this fails I'll need to add a loop to extract resource arrays
 		return '\n'.join(ret)
 
 class DDSHeader(object):
@@ -416,6 +508,11 @@ def convert_dx10(fp, header):
 	convert(fp, header, dtype)
 
 def convert_fourcc(fp, header):
+	# NOTE: Documentation notes that DDS files should not store the format
+	# D3DFORMAT / DXGI_FORMAT here as they cannot be distinguished. But
+	# it seems they do anyway, so support what we can. Might need to add a
+	# flag to choose between the two if this becomes a problem in practice.
+
 	if header.pixel_format.format not in d3d9_pixel_formats:
 		return
 
