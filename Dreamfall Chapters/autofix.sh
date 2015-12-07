06aa3821c10c63c0df34a965c62de26bc6be5eeb
Ian Munsie <darkstarsword@gmail.com>
2015-12-07 20:17:54 +1100

    Dreamfall Chapters: Rerun auto halo fix to catch other bugs
    Most of the diff is purely cosmetic, but there are a couple here that
    had real bugs that have been fixed.
    
    Also redid shaders that were missing --add-fog-on-sm3-update (only
    changed one)

--- Dreamfall Chapters/autofix.sh
+++ Dreamfall Chapters/autofix.sh
@@ -7,6 +7,6 @@ cd extracted
 "$DIR/extract_unity_shaders.py" */*.shader --type=d3d9
 cd ShaderCRCs
 
-"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --only-autofixed */vp/*.txt
+"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed */vp/*.txt
 "$DIR/shadertool.py" -I ../.. --fix-unity-lighting-ps --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/*
 "$DIR/shadertool.py" -I ../.. --disable=0 --condition=c200.x Hidden_Coord/*/* Hidden_Depth/*/* Hidden_DepthBreaks/*/* Hidden_Final\ Interpolation/*/* Hidden_InterpolateAlongRays/*/* Hidden_Raymarch/*/*
