4461c891e68a455d645f329f99fb6106c5ccaa3d
Ian Munsie <darkstarsword@gmail.com>
2015-12-08 16:56:27 +1100

    Dreamfall Chapters: Correct _WorldSpaceCameraPos by script
    Phew! That sure is a lot of shaders the autofix picked up. Doing some
    spot checks I can't see any breakages as a result of this and I can see
    more fixed reflections.
    
    I call that a win.

--- Dreamfall Chapters/autofix.sh
+++ Dreamfall Chapters/autofix.sh
@@ -7,6 +7,11 @@ cd extracted
 "$DIR/extract_unity_shaders.py" */*.shader --type=d3d9
 cd ShaderCRCs
 
-"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed */vp/*.txt
-"$DIR/shadertool.py" -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/*
+# Order from most specific targets to least specific targets
 "$DIR/shadertool.py" -I ../.. --disable=0 --condition=c200.x Hidden_Coord/*/* Hidden_Depth/*/* Hidden_DepthBreaks/*/* Hidden_Final\ Interpolation/*/* Hidden_InterpolateAlongRays/*/* Hidden_Raymarch/*/*
+"$DIR/shadertool.py" -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/*
+"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed */vp/*.txt
+
+# TODO: Combine --auto-fix-vertex-halo and --fix-unity-reflection
+"$DIR/shadertool.py" -I ../.. --fix-unity-reflection --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt >> ../../DX9Settings.ini
+"$DIR/shadertool.py" -I ../.. --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt >> ../../DX9Settings.ini
