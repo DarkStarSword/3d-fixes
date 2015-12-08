2566c11d48c77cd6ebcbc1cafaded6facaf5de6b
Ian Munsie <darkstarsword@gmail.com>
2015-12-08 17:35:25 +1100

    Dreamfall Chapters: Apply _WorldSpaceCameraPos fix to shaders with halo fix
    Doing these separately to the last batch to make it easier to see if the
    tool has done the right thing when combining the two fixes. Looks good -
    declarations are being shared. Could potentially make the script share
    more temp regs and the texldl, but that's a minor issue.
    
    Removed sections to get the MVP and _Object2World matrices that were not
    generated by the script - they were fine, but there would have been some
    overlap since this was re-run on shaders already in the ShaderOverride
    folder, and we probably don't need the extras that don't themselves
    include the fix.

--- Dreamfall Chapters/autofix.sh
+++ Dreamfall Chapters/autofix.sh
@@ -9,9 +9,6 @@ cd ShaderCRCs
 
 # Order from most specific targets to least specific targets
 "$DIR/shadertool.py" -I ../.. --disable=0 --condition=c200.x Hidden_Coord/*/* Hidden_Depth/*/* Hidden_DepthBreaks/*/* Hidden_Final\ Interpolation/*/* Hidden_InterpolateAlongRays/*/* Hidden_Raymarch/*/*
-"$DIR/shadertool.py" -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/*
-"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed */vp/*.txt
-
-# TODO: Combine --auto-fix-vertex-halo and --fix-unity-reflection
-"$DIR/shadertool.py" -I ../.. --fix-unity-reflection --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt >> ../../DX9Settings.ini
+"$DIR/shadertool.py" -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/* >> ../../DX9Settings.ini
+"$DIR/shadertool.py" -I ../.. --auto-fix-vertex-halo --fix-unity-reflection --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt >> ../../DX9Settings.ini
 "$DIR/shadertool.py" -I ../.. --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt >> ../../DX9Settings.ini
