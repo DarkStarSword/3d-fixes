e7cb62913be0e917d25d435e1d712291487e35b5
Ian Munsie <darkstarsword@gmail.com>
2015-12-15 02:16:25 +1100

    The Forest: Update ceto autofix
    - Apply lessons learned from Stranded Deep. Hasn't caused an issue here
      yet, and the Ceto shader is a bit different here, but since we know
      applying one fix to all pixel shaders was a bad idea in Stranded Deep
      let's perform the same type of filtering here as well. Apply different
      halo fixes to different pixel shaders to match what would have been
      applied in the corresponding vertex shader.
    
    - Add reflection fix

--- The Forest/autofix.sh
+++ The Forest/autofix.sh
@@ -19,9 +19,14 @@ find . \( -name 05F7E52C.txt -o -name 678DC18B.txt \) -a -print0 | xargs -0 dirn
 find -maxdepth 1 -print0 | grep -zv '\(CetoTF_OceanTopSide\|CetoTF_OceanUnderSide\|Ceto_OceanMask\)' | sed -z 's/$/\/vp\/*/' | xargs -0 \
 	shadertool.py -I ../.. --stereo-sampler-vs=s3 --auto-fix-vertex-halo --fix-unity-reflection --add-fog-on-sm3-update --only-autofixed | unix2dos | tee -a ../../DX9Settings.ini
 
-# Water halo fix in the pixel shader:
-shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection  --adjust-input=texcoord4 --adjust-input=texcoord5 --adjust-multiply=0.5 --ignore-other-errors CetoTF_OceanTopSide*/fp/* | unix2dos | tee -a ../../DX9Settings.ini
-shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection  --adjust-input=texcoord4 --adjust-input=texcoord5 --adjust-multiply=0.5 --ignore-other-errors CetoTF_OceanUnderSide*/fp/* | unix2dos | tee -a ../../DX9Settings.ini
+# Water halo fix in the pixel shader (with lessons learned from Stranded Deep -
+# check vertex shaders to work out which texcoords to adjust and filter by keyword):
+grep -lZ '^\/\/.*Keywords {.*SHADOWS_OFF' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord5 --adjust-multiply=0.5 --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
+grep -lZ '^\/\/.*Keywords {.*SHADOWS_SCREEN' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord8 --adjust-multiply=0.5 --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
+grep -lZ '^\/\/.*Keywords {.*SHADOWS_DEPTH' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
 
 # Reflection fix:
 shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
