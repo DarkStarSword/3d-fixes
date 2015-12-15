846d9540642c4414ba3146bfbcb63fcbbc12efbb
Ian Munsie <darkstarsword@gmail.com>
2015-12-15 14:06:00 +1100

    The Forest autofix.sh: Re-enable writing the DX9Settings.ini file
    Before I forget. I should probably script something easier to turn on
    and off for this, and maybe make it intelligent enough to see that a
    section is already in the ini file...

--- The Forest/autofix.sh
+++ The Forest/autofix.sh
@@ -22,11 +22,11 @@ find -maxdepth 1 -print0 | grep -zv '\(CetoTF_OceanTopSide\|CetoTF_OceanUnderSid
 # Water halo fix in the pixel shader (with lessons learned from Stranded Deep -
 # check vertex shaders to work out which texcoords to adjust and filter by keyword):
 grep -lZ '^\/\/.*Keywords {.*SHADOWS_OFF' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
-	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord5 --adjust-multiply=0.5 --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord5 --adjust-multiply=0.5 --ignore-other-errors | unix2dos | tee -a ../../DX9Settings.ini
 grep -lZ '^\/\/.*Keywords {.*SHADOWS_SCREEN' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
-	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord8 --adjust-multiply=0.5 --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord4 --adjust-input=texcoord8 --adjust-multiply=0.5 --ignore-other-errors | unix2dos | tee -a ../../DX9Settings.ini
 grep -lZ '^\/\/.*Keywords {.*SHADOWS_DEPTH' CetoTF_Ocean{Top,Under}Side_*/fp/*.txt | xargs -0 \
-	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord --ignore-other-errors #| unix2dos | tee -a ../../DX9Settings.ini
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --adjust-unity-ceto-reflections --adjust-input=texcoord --ignore-other-errors | unix2dos | tee -a ../../DX9Settings.ini
 
 # Reflection fix:
 shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
