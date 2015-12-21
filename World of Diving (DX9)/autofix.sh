7e40847ce9abc204ad928600bad7a41f6c5cf7fe
Ian Munsie <darkstarsword@gmail.com>
2015-12-21 22:52:35 +1100

    World of Diving: Switch stereo samplers
    Some surfaces display green in one eye and blue in the other in this
    game. I suspect it's yet another instance of Helix Mod breaking shaders
    by not restoring the original sampler, so switch it to use samplers that
    the game never does.

--- World of Diving (DX9)/autofix.sh
+++ World of Diving (DX9)/autofix.sh
@@ -12,10 +12,10 @@ cleanup_unity_shaders.py ../..
 
 # Lighting fix - match any shaders that use known lighting vertex shaders:
 find . \( -name 05F7E52C.txt -o -name 678DC18B.txt \) -a -print0 | xargs -0 dirname -z | sed -z 's/vp$/fp\/*/' | xargs -0 \
-	shadertool.py -I ../.. --fix-unity-lighting-ps-world --only-autofixed | unix2dos | tee -a ../../DX9Settings.ini
+	shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-lighting-ps-world --only-autofixed | unix2dos | tee -a ../../DX9Settings.ini
 
 # Vertex shader halo and reflection fix:
-shadertool.py -I ../.. --fix-unity-reflection --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
+shadertool.py -I ../.. --stereo-sampler-vs=s3 --fix-unity-reflection --auto-fix-vertex-halo --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
 
 # Pixel shader reflection fix:
-shadertool.py -I ../.. --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
+shadertool.py -I ../.. --stereo-sampler-ps=s15 --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
