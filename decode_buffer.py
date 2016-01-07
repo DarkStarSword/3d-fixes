d3c34bbb9794ae3982a76ccf10bd96fb9da4b751
Ian Munsie <darkstarsword@gmail.com>
2016-01-08 00:14:09 +1100

    decode_buffer.py: Add --truncate option to skip tailing 0s
    Some of the buffers I'm looking at are significantly larger than the
    data they contain, so add an option to truncate tailing 0s so we can
    focus on what is important.

--- decode_buffer.py
+++ decode_buffer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, os, struct, itertools
+import sys, os, struct, itertools, io
 import argparse
 
 try:
@@ -9,13 +9,20 @@ except ImportError:
 	float_to_hex = None
 
 def dump(stream, args):
+	last_non_zero = 0
+	if args.truncate:
+		out = io.StringIO()
+	else:
+		out = sys.stdout
+
 	for index in itertools.count():
+		zero = True
 		for offset in range(args.stride // 4):
 			buf = stream.read(4)
 			if len(buf) == 0:
 				return
 			if len(buf) < 4: 
-				print('Remaining:', repr(buf))
+				print('Remaining:', repr(buf), file=out)
 				return
 
 			if float_to_hex is not None:
@@ -27,18 +34,26 @@ def dump(stream, args):
 			ival, = struct.unpack('<i', buf)
 			uval, = struct.unpack('<I', buf)
 
+			if uval:
+				zero = False
+
 			if args.stride == 1:
-				print('{:08x}: '.format((index * args.stride + offset) * 4), end='')
+				print('{:08x}: '.format((index * args.stride + offset) * 4), end='', file=out)
 			else:
-				print('{}+{:04x}: '.format(index, offset * 4), end='')
-			print('0x{:08x} | {: 12d} | {}'.format(uval, ival, fval))
+				print('{}+{:04x}: '.format(index, offset * 4), end='', file=out)
+			print('0x{:08x} | {: 12d} | {}'.format(uval, ival, fval), file=out)
+		if args.truncate and not zero:
+			print(out.getvalue(), end='')
+			out.seek(0)
+			out.truncate(0)
 		if args.stride != 1:
-			print()
+			print(file=out)
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('files', nargs='+', type=argparse.FileType('rb'))
 	parser.add_argument('-s', '--stride', type=int, default=1)
+	parser.add_argument('-t', '--truncate', action='store_true')
 	args = parser.parse_args()
 
 	for file in args.files:
