a54e84dfe1f2ecb84475d9794ade01d20c370e64
Ian Munsie <darkstarsword@gmail.com>
2016-02-23 06:33:00 +1100

    Proof of concept to convert games to SBS / TAB
    Here's a proof of concept converting a game to SBS / TAB with 3DMigoto.
    This only solves one of the issues - the driver will still be driving
    the display as 3D Vision (meaning the 720p limit still applies with 3D
    TV Play) and if it cannot force SBS/TAB in that mode it will not work.
    It also does not support full frame SBS/TAB, only half-packed. EDID
    overrides may help, but I haven't tried. I am not working on this
    actively, there is no ETA as to when or if it will be usable. Edit:
    joker18 and DHR figured out an easy solution to the output problem by
    using checkerboard mode, so this actually works now :)

---
+++ 3DVision2SBS/ShaderFixes/3dvision2sbs.hlsl
@@ -0,0 +1,41 @@
+Texture2D<float4> StereoParams : register(t125);
+Texture1D<float4> IniParams : register(t120);
+Texture2D<float4> t100 : register(t100);
+
+void main(float4 pos : SV_Position0, out float4 result : SV_Target0)
+{
+	float4 stereo = StereoParams.Load(0);
+	float mode = IniParams.Load(int2(7, 0)).x;
+
+	float x = pos.x;
+	float y = pos.y;
+	float width, height;
+
+	t100.GetDimensions(width, height);
+
+	if (mode == 0) { // Regular 3D Vision
+		if (stereo.z == 1)
+			x += width / 2;
+	} else if (mode == 1) { // Regular 3D Vision with eyes swapped
+		if (stereo.z == -1)
+			x += width / 2;
+	} else if (mode == 2 || mode == 3) { // Side by side
+		x *= 2;
+		if (mode == 3) { // Swap eyes
+			x += width / 2 * (x >= width / 2 ? -1 : 1);
+		}
+	} else if (mode == 4 || mode == 5) { // Top and bottom
+		y *= 2;
+		if (y >= height) {
+			y -= height;
+			if (mode == 4) {
+				x += width / 2;
+			}
+		} else if (mode == 5) {
+			x += width / 2;
+		}
+	}
+
+	result = t100.Load(float3(x, y, 0));
+	result.w = 1;
+}
---
+++ 3DVision2SBS/ShaderFixes/full_screen.hlsl
@@ -0,0 +1,32 @@
+void main(
+		out float4 pos : SV_Position0,
+		uint vertex : SV_VertexID)
+{
+	// Not using vertex buffers so manufacture our own coordinates.
+	// You may have to adjust this depending on whether the game is using
+	// clockwise or counterclockwise for front-facing surfaces:
+	switch(vertex) {
+		case 0:
+			pos.xy = float2(1, -1);
+			break;
+		case 1:
+			pos.xy = float2(-1, -1);
+			break;
+		case 2:
+			pos.xy = float2(-1, 1);
+			break;
+		case 3:
+			pos.xy = float2(-1, 1);
+			break;
+		case 4:
+			pos.xy = float2(1, -1);
+			break;
+		case 5:
+			pos.xy = float2(1, 1);
+			break;
+		default:
+			pos.xy = 0;
+			break;
+	};
+	pos.zw = float2(0, 1);
+}
