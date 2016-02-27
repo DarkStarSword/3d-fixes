4a9f48e30744834089a9709f1af4d870eb974fb1
Ian Munsie <darkstarsword@gmail.com>
2016-02-27 16:54:41 +1100

    3DVision2SBS: Average both samples to reduce aliasing
    Since this mode only uses half-packed SBS / TAB, half the pixels are
    lost, which creates aliasing. To reduce this, average two pixels
    together so that the result will be smoother.

--- 3DVision2SBS/ShaderFixes/3dvision2sbsps.hlsl
+++ 3DVision2SBS/ShaderFixes/3dvision2sbsps.hlsl
@@ -10,6 +10,7 @@ void main(float4 pos : SV_Position0, out float4 result : SV_Target0)
 	float x = pos.x;
 	float y = pos.y;
 	float width, height;
+	float x1 = 0, y1 = 0;
 
 	t100.GetDimensions(width, height);
 
@@ -21,11 +22,13 @@ void main(float4 pos : SV_Position0, out float4 result : SV_Target0)
 			x += width / 2;
 	} else if (mode == 2 || mode == 3) { // Side by side
 		x *= 2;
+		x1 = 1;
 		if (mode == 3) { // Swap eyes
 			x += width / 2 * (x >= width / 2 ? -1 : 1);
 		}
 	} else if (mode == 4 || mode == 5) { // Top and bottom
 		y *= 2;
+		y1 = 1;
 		if (y >= height) {
 			y -= height;
 			if (mode == 4) {
@@ -37,5 +40,7 @@ void main(float4 pos : SV_Position0, out float4 result : SV_Target0)
 	}
 
 	result = t100.Load(float3(x, y, 0));
+	if (x1 || y1)
+		result = (result + t100.Load(float3(x + x1, y + y1, 0))) / 2;
 	result.w = 1;
 }
