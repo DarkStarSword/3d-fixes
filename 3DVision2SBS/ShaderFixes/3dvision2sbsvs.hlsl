09a17e3f89d126225d7599484474c8e5a8e88838
Ian Munsie <darkstarsword@gmail.com>
2016-02-28 04:39:36 +1100

    3DVision2SBS: Use triangle strip topology
    3DMigoto 1.2.32 will allow the primitive topology to be overridden,
    which allows us to reliably set the correct topology for the vertices we
    generate. Switch to a triangle strip topology, as that allows a full
    screen effect to be constructed with four vertices instead of six, as it
    implicitly shares two of the vertices between both triangles.

--- 3DVision2SBS/ShaderFixes/3dvision2sbsvs.hlsl
+++ 3DVision2SBS/ShaderFixes/3dvision2sbsvs.hlsl
@@ -13,25 +13,17 @@ void main(
 	}
 
 	// Not using vertex buffers so manufacture our own coordinates.
-	// You may have to adjust this depending on whether the game is using
-	// clockwise or counterclockwise for front-facing surfaces:
 	switch(vertex) {
 		case 0:
-			pos.xy = float2(1, -1);
-			break;
-		case 1:
 			pos.xy = float2(-1, -1);
 			break;
+		case 1:
+			pos.xy = float2(-1, 1);
+			break;
 		case 2:
-			pos.xy = float2(-1, 1);
-			break;
-		case 3:
-			pos.xy = float2(-1, 1);
-			break;
-		case 4:
 			pos.xy = float2(1, -1);
 			break;
-		case 5:
+		case 3:
 			pos.xy = float2(1, 1);
 			break;
 		default:
