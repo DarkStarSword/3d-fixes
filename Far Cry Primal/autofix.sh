d2e5c19fef045dba8c255c592d0a7a5cd07cd4c5
Ian Munsie <darkstarsword@gmail.com>
2016-03-07 02:00:05 +1100

    FCPrimal: Run camera position fix on all shaders with a cube map reflection
    This should fix the remaining fake reflection water shaders.

--- Far Cry Primal/autofix.sh
+++ Far Cry Primal/autofix.sh
@@ -9,4 +9,6 @@ asmtool.py -i --fix-fcprimal-physical-lighting --only-autofixed ShaderCache/*-cs
 # --fix-fcprimal-camera-pos improves a few subtle things, but must not be
 # applied to vertex shaders. It's not clear that it's worth applying
 # universally, so just adding it to wherever it makes a difference. The
-# physical lighting fix implies it.
+# physical lighting fix implies it, so no need to do so explicitly there. Apply
+# it to all shaders that include a cube map reflection:
+asmtool.py -i --fix-fcprimal-camera-pos $(grep -l ReflectionCubeTexture__TexObj__ ShaderCache/*-ps.txt)
