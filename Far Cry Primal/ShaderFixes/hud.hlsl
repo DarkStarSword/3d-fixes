8be0149bc8f70c9fdaa5a350266192d58122506c
Ian Munsie <darkstarsword@gmail.com>
2016-03-06 16:13:15 +1100

    FCPrimal: HUD tweaks: Subtitles & texture filtering
    The subtitles overlap with the minimap, which can distort them. Add a
    pixel shader trigger to distinguish between the two and allow subtitles
    to be moved to their own depth.
    
    Add texture filtering to begin whitelisting elements to automatically
    adjust. For now only the dot crosshair is whitelisted.

--- Far Cry Primal/ShaderFixes/hud.hlsl
+++ Far Cry Primal/ShaderFixes/hud.hlsl
@@ -1,8 +1,13 @@
 #include <ShaderFixes/crosshair.hlsl>
 
-bool is_minimap(float4 pos)
+bool is_minimap(float4 pos, float4 params1)
 {
-	return pos.x < -0.4 && pos.y < -0.05;
+	return params1.x && pos.x < -0.4 && pos.y < -0.05;
+}
+
+bool is_subtitle(float4 pos)
+{
+	return pos.y < -0.7;
 }
 
 bool is_goals(float4 pos)
@@ -10,21 +15,33 @@ bool is_goals(float4 pos)
 	return pos.x < 0.15 && pos.y > 0.2;
 }
 
+bool is_crosshair(float4 pos, float4 params1)
+{
+	return (params1.y == 2);
+}
+
 float adjust_hud(float4 pos)
 {
 	float4 stereo = StereoParams.Load(0);
-	float4 params = IniParams.Load(0);
+	float4 params0 = IniParams.Load(0);
+	float4 params1 = IniParams.Load(1);
 	float width, height;
 
 	ZBuffer.GetDimensions(width, height);
 	if (!width)
-		return stereo.x * params.x;
+		return stereo.x * params0.x;
 
-	if (is_minimap(pos))
-		return stereo.x * params.y;
+	if (is_minimap(pos, params1))
+		return stereo.x * params0.y;
+
+	if (is_subtitle(pos))
+		return stereo.x * params0.w;
 
 	if (is_goals(pos))
-		return stereo.x * params.z - abs(stereo.x);
+		return stereo.x * params0.z - abs(stereo.x);
 
-	return adjust_from_stereo2mono_depth_buffer(0, 0);
+	if (is_crosshair(pos, params1))
+		return adjust_from_stereo2mono_depth_buffer(0, 0);
+
+	return stereo.x * params0.x;
 }
