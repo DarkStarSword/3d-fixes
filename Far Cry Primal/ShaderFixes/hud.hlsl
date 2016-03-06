b2c7c07791b4aae91c082e66f76e87dafb11b855
Ian Munsie <darkstarsword@gmail.com>
2016-03-06 18:49:45 +1100

    FCPrimal: Disable texture filtering + more HUD tweaks
    FCPrimal suffers the same issue as FC4 with texture filtering, and while
    I can counter that with track_texture_updates, but given that I'm
    planning to rework that let's hold off on adding texture filtering.
    
    In the meantime, restrict the crosshair adjustment to the center of the
    screen, and only from the shader responsible for the crosshair.
    
    Add the depth buffer copy to other HUD shaders that were missing it &
    fix the mip-map detection being inverted.

--- Far Cry Primal/ShaderFixes/hud.hlsl
+++ Far Cry Primal/ShaderFixes/hud.hlsl
@@ -2,7 +2,7 @@
 
 bool is_minimap(float4 pos, float4 params1)
 {
-	return params1.x && pos.x < -0.4 && pos.y < -0.05;
+	return !params1.x && pos.x < -0.4 && pos.y < -0.05;
 }
 
 bool is_subtitle(float4 pos)
@@ -17,15 +17,22 @@ bool is_goals(float4 pos)
 
 bool is_crosshair(float4 pos, float4 params1)
 {
-	return (params1.y == 2);
+	return (all(abs(pos.xy) < 0.10 * pos.w));
+
+	// Let's wait until I've reworked the texture hashes for a faster
+	// track_resource_updates before we use texture filtering:
+	// return (params1.y == 2);
 }
 
-float adjust_hud(float4 pos)
+float adjust_hud(float4 pos, bool crosshair_shader)
 {
 	float4 stereo = StereoParams.Load(0);
 	float4 params0 = IniParams.Load(0);
-	float4 params1 = IniParams.Load(1);
+	float4 params1 = IniParams.Load(int2(1, 0));
 	float width, height;
+
+	if (params1.z) // Menu background
+		return stereo.x * params0.x;
 
 	ZBuffer.GetDimensions(width, height);
 	if (!width)
@@ -40,7 +47,7 @@ float adjust_hud(float4 pos)
 	if (is_goals(pos))
 		return stereo.x * params0.z - abs(stereo.x);
 
-	if (is_crosshair(pos, params1))
+	if (crosshair_shader && is_crosshair(pos, params1))
 		return adjust_from_stereo2mono_depth_buffer(0, 0);
 
 	return stereo.x * params0.x;
