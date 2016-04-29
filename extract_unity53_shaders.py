73d3f9baf037f2bf46386272a64fae89cb5ad61b
Ian Munsie <darkstarsword@gmail.com>
2016-04-29 20:42:39 +1000

    extract_unity53_shaders: Handle variable size header, Better DX9 support
    The variable size header on the bind info is still a bit of a mystery,
    but it seems to end in a reasonably consistent manner depending on the
    DX version. Use that as the best guess to find the start of the bind
    info clauses.
    
    Some rarer types (VectorInt, ScalarBool) were also added - pretty sure
    this can be more generic between the common ones and these, but for now
    using asserts so I catch any variations of these.
    
    With this change, this can now extract headers for all shaders in The
    Long Dark, both DX11 and DX9. It still lacks a way to distinguish
    between vertex and pixel shaders.

--- extract_unity53_shaders.py
+++ extract_unity53_shaders.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-import sys, os, argparse, glob, struct
+import sys, os, argparse, glob, struct, zlib
 import extract_unity_shaders
 
 class ParseError(Exception): pass
+
+class ShaderType(object):
+    dx9 = 1
+    dx11 = 2
+    opengl = 3
 
 def align(file, alignment):
     off = file.tell()
@@ -12,54 +17,81 @@ def align(file, alignment):
         return
     file.seek(alignment - mod, 1)
 
+def consume_until_double_zero(file):
+    ret = []
+    while True:
+        tmp = struct.unpack('<2I', file.read(8))
+        if tmp == (0, 0):
+            return ret
+        ret.extend(tmp)
+
+def consume_until_dx11_num_sections(file, undeciphered3):
+    data = consume_until_double_zero(file)
+    undeciphered3.extend(data[:-2])
+    num_sections = data[-2]
+    assert(data[-1] == 0)
+    return num_sections
+
 def add_header(headers, header):
     headers.append(header)
     print(header)
 
-def decode_consts(file, headers, is_dx11):
+def decode_consts(file, headers, shader_type):
     (num_entries,) = struct.unpack('<I', file.read(4))
     for j in range(num_entries):
         (name_len,) = struct.unpack('<I', file.read(4))
         entry_name = file.read(name_len).decode('ascii')
         align(file, 4)
-        (zero, type_size1, type_size2, type3, one, offset) = struct.unpack('<6I', file.read(24))
-        assert(zero == 0)
+        (type1, type_size1, type_size2, type3, one, offset) = struct.unpack('<6I', file.read(24))
         assert(one == 1)
-        if type_size1 == 1:
-            assert(type3 == 0)
-            if type_size2 == 1:
-                add_header(headers, 'Float {} [{}]'.format(offset, entry_name))
-            elif type_size2 == 4:
-                add_header(headers, 'Vector {} [{}]'.format(offset, entry_name))
-            elif type_size2 == 3:
-                add_header(headers, 'Vector {} [{}] {}'.format(offset, entry_name, type_size2))
+        if type1 == 0: # Float
+            if type_size1 == 1:
+                assert(type3 == 0)
+                if type_size2 == 1:
+                    add_header(headers, 'Float {} [{}]'.format(offset, entry_name))
+                elif type_size2 == 4:
+                    add_header(headers, 'Vector {} [{}]'.format(offset, entry_name))
+                elif type_size2 in (2, 3):
+                    add_header(headers, 'Vector {} [{}] {}'.format(offset, entry_name, type_size2))
+                else:
+                    raise ParseError('Unknown type_size2: {} for {}, type_size1: {}'.format(type_size2, entry_name, type_size1))
+            elif type_size1 == 4:
+                assert(type3 == 1)
+                if type_size2 == 4:
+                    add_header(headers, 'Matrix {} [{}]'.format(offset, entry_name))
+                elif type_size2 in (2, 3): # 2x4 unconfirmed for DX11
+                    add_header(headers, 'Matrix {} [{}] {}'.format(offset, entry_name, type_size2))
+                    assert(shader_type == ShaderType.dx11) # Need to check syntax for other way around
+                else:
+                    assert(False)
+            elif type_size1 in (2, 3):
+                assert(type_size2 == 4)
+                assert(type3 == 1)
+                add_header(headers, 'Matrix {} [{}] {}'.format(offset, entry_name, type_size1))
+                assert(shader_type == ShaderType.dx9) # Need to check syntax for other way around
             else:
-                raise ParseError('Unknown type_size2: {} for {}, type_size1: {}'.format(type_size2, entry_name, type_size1))
-        elif type_size1 == 4:
-            assert(type3 == 1)
-            if type_size2 == 4:
-                add_header(headers, 'Matrix {} [{}]'.format(offset, entry_name))
-            elif type_size2 in (2, 3): # 2x4 unconfirmed for DX11
-                add_header(headers, 'Matrix {} [{}] {}'.format(offset, entry_name, type_size2))
-                assert(is_dx11) # Need to check syntax for other way around
-            else:
-                assert(False)
-        elif type_size1 in (2, 3):
+                raise ParseError('Unknown name: {} type_size1: {} type_size2: {} type3: {} offset: {}'.format(entry_name, type_size1, type_size2, type3, offset))
+        elif type1 == 1: # Int
+            assert(type_size1 == 1)
             assert(type_size2 == 4)
-            assert(type3 == 1)
-            add_header(headers, 'Matrix {} [{}] {}'.format(offset, entry_name, type_size1))
-            assert(not is_dx11) # Need to check syntax for other way around
+            assert(type3 == 0)
+            add_header(headers, 'VectorInt {} [{}] {}'.format(offset, entry_name, type_size2))
+        elif type1 == 2: # Bool
+            assert(type_size1 == 1)
+            assert(type_size2 == 1)
+            assert(type3 == 0)
+            add_header(headers, 'ScalarBool {} [{}]'.format(offset, entry_name))
         else:
-            raise ParseError('Unknown name: {} type_size1: {} type_size2: {} type3: {} offset: {}'.format(entry_name, type_size1, type_size2, type3, offset))
+            raise ParseError('Unknown name: {} type1: {} type_size1: {} type_size2: {} type3: {} offset: {}'.format(entry_name, type1, type_size1, type_size2, type3, offset))
 
-def decode_constbuffers(file, num_cbs, headers, is_dx11):
+def decode_constbuffers(file, num_cbs, headers, shader_type):
     for i in range(num_cbs):
         (name_len,) = struct.unpack('<I', file.read(4))
         cb_name = file.read(name_len).decode('ascii')
         align(file, 4)
         (cb_size,) = struct.unpack('<I', file.read(4))
         add_header(headers, 'ConstBuffer "{}" {}'.format(cb_name, cb_size))
-        decode_consts(file, headers, is_dx11)
+        decode_consts(file, headers, shader_type)
 
 def decode_binds(file, headers):
     (num_binds,) = struct.unpack('<I', file.read(4))
@@ -83,11 +115,12 @@ def decode_binds(file, headers):
             assert(False)
 
 def decode_dx9_bind_info(file, headers):
-    decode_consts(file, headers, False)
+    decode_consts(file, headers, ShaderType.dx9)
     decode_binds(file, headers)
 
 def decode_dx11_bind_info(file, num_sections, headers):
-    decode_constbuffers(file, num_sections-1, headers, True)
+    print('num dx11 sections: {0}'.format(num_sections))
+    decode_constbuffers(file, num_sections-1, headers, ShaderType.dx11)
     decode_binds(file, headers)
 
 def extract_shader_at(file, offset, size):
@@ -95,13 +128,9 @@ def extract_shader_at(file, offset, size):
     headers = []
     file.seek(offset)
     try:
-        (u1, u2, u3, u4, u5, num_keywords) = struct.unpack('<6I', file.read(24))
-        print('  u1: {0} (0x{0:08x})'.format(u1))
+        (u1, u2, u3, u4, u5, num_keywords) = struct.unpack('<6i', file.read(24))
         assert(u1 == 0x0c02c8a6)
-        print('  u2: {0} (0x{0:08x})'.format(u2)) # 15, 16, 17, 18... shader type? at a guess, 17=vs? 15=ps?
-        print('  u3: {0} (0x{0:08x})'.format(u3)) # Anything between 1 and 94?
-        print('  u4: {0} (0x{0:08x})'.format(u4)) # Anything between 0 and 9?, sometimes 0xffffffff
-        print('  u5: {0} (0x{0:08x})'.format(u5)) # usually 0, sometimes 1?, sometimes 0xffffffff
+        add_header(headers, 'undeciphered1: {} {} {} {}'.format(u2, u3, u4, u5))
 
         keywords = []
         for i in range(num_keywords):
@@ -111,99 +140,73 @@ def extract_shader_at(file, offset, size):
         if keywords:
             add_header(headers, 'Keywords { "%s" }' % '" "'.join(keywords))
 
-        (shader_size, u8a, u8b, u8c, u8d, u8e) = struct.unpack('<I5B', file.read(9))
-        print('  shader size: {0} (0x{0:08x})'.format(shader_size))
-        print('  u8: {:02x} {:02x} {:02x} {:02x} {:02x}'.format(u8a, u8b, u8c, u8d, u8e)) # Think this is related to the bindings
+        (shader_size, u8a, u8b, u8c, u8d, u8e) = struct.unpack('<i5b', file.read(9))
+        # print('  shader size: {0} (0x{0:08x})'.format(shader_size))
+        add_header(headers, 'undeciphered2: {} {} {} {} {}'.format(u8a, u8b, u8c, u8d, u8e)) # Think this is related to the bindings
 
         shader = file.read(shader_size)
         align(file, 4)
-        # TODO: DX9
-        hash = '%016x' % extract_unity_shaders.fnv_3Dmigoto_shader(shader)
-        dest = '%s--s' % hash
+        if shader[:4] == b'DXBC': # FIXME: Better way to detect this?
+            shader_type = ShaderType.dx11
+            hash = '%016x' % extract_unity_shaders.fnv_3Dmigoto_shader(shader)
+            dest = '%s--s' % hash
+            assert(u4 >= 0)
+            assert(u5 >= 0)
+            assert(u8c >= 0)
+            assert(u8d >= 0)
+            assert(u8e >= 0)
+        elif shader[3:7] == b'CTAB': # DX9
+            shader_type = ShaderType.dx9
+            hash = '%08x' % zlib.crc32(shader)
+            dest = '%s' % hash
+            # assert(u5 == -1) - no, there are exceptions
+            assert(u8d == -1)
+            assert(u8e == -2)
+            # if u4 == -1: - no, these are independent
+            #   assert(u8c == -2)
+            assert(u8c == -1 or u8c == -2)
+        else: # OpenGL?
+            shader_type = ShaderType.opengl
+            print('Skipping OpenGL(?) shader')
+            dest = 'gl-%x' % offset
         print('Extracting %s.bin...' % dest)
         with open(dest + '.bin', 'wb') as out:
             out.write(shader)
 
-        if False: # Useful for debugging
+        if True: # Useful for debugging
             bind_info_size = size - (file.tell() - offset)
-            print('  Remaining %i bytes' % bind_info_size)
             pos = file.tell()
             with open(dest + '.rem', 'wb') as out:
                 out.write(file.read(bind_info_size))
             file.seek(pos)
 
         # Have not fully deciphered the data around this point, and depending
-        # on the values the size can vary. For now use many asserts to catch
-        # any deviations for analysis.
-        (u10,) = struct.unpack('<I', file.read(4))
-        print('  u10: {0}'.format(u10))
-        if u10 == 0:
-            (u11,) = struct.unpack('<I', file.read(4))
-            print('  u11: {0}'.format(u11))
-            if u11 == 0: # DX9 only?
-                # DX9 Vertex shades?
-                (u12, u13, u14, u15, u16) = struct.unpack('<5I', file.read(20))
-                print('  u12: {0}'.format(u12))
-                print('  u13: {0}'.format(u13))
-                print('  u14: {0}'.format(u14))
-                print('  u15: {0}'.format(u15))
-                print('  u16: {0}'.format(u16))
-                assert(u12 in (1, 3))
-                assert(u13 in (2, 3, 4))
-                assert(u14 == 1)
-                assert(u15 == 0)
-                assert(u16 == 0)
+        # on the values the size can vary. Making a best guess based on
+        # obsevations that DX9 ends in 0, 0 and DX11 ends in num_sections, 0,
+        # 0, 0. If first value != 0 in DX11, it is followed by two 0s.
+        # Values seem to be in pairs. Suspect they are related to the generic
+        # "Bind" statements in previous Unity versions, but can't make heads or
+        # tails of them.
+        undeciphered3 = list(struct.unpack('<I', file.read(4)))
 
-                decode_dx9_bind_info(file, headers)
-                assert(file.tell() - size - offset == 0)
-            else:
-                # DX11 Pixel shaders?
-                num_sections = u11
-                (u12, u13, u14) = struct.unpack('<3I', file.read(12))
-                print('  num_sections: {0}'.format(num_sections))
-                print('  u12: {0}'.format(u12))
-                print('  u13: {0}'.format(u13))
-                print('  u14: {0}'.format(u14))
-                assert(u12 == 0)
-                assert(u13 == 0)
-                assert(u14 == 0)
+        if shader_type == ShaderType.dx11:
+            if undeciphered3[0]:
+                undeciphered3.extend(struct.unpack('<2I', file.read(8)))
 
-                decode_dx11_bind_info(file, num_sections, headers)
-                assert(file.tell() - size - offset == 0)
+            num_sections = consume_until_dx11_num_sections(file, undeciphered3)
 
-        elif (u10 == 1): # DX9 only?
-            # DX9 Pixel shaders?
-            (u11, u12) = struct.unpack('<2I', file.read(8))
-            print('  u11: {0}'.format(u11))
-            print('  u12: {0}'.format(u12))
-            assert(u11 == 0)
-            assert(u12 == 0)
-
-            decode_dx9_bind_info(file, headers)
-            assert(file.tell() - size - offset == 0)
-        elif (u10 == 2):
-            # DX11 Vertex Shaders?
-            (u11, u12, u13, u14, num_sections, u16, u17, u18) = struct.unpack('<8I', file.read(32))
-            print('  u11: {0}'.format(u11))
-            print('  u12: {0}'.format(u12))
-            print('  u13: {0}'.format(u13))
-            print('  u14: {0}'.format(u14))
-            print('  num_sections: {0}'.format(num_sections))
-            print('  u16: {0}'.format(u16))
-            print('  u17: {0}'.format(u17))
-            print('  u18: {0}'.format(u18))
-            assert(u11 == 0)
-            assert(u12 == 0)
-            assert(u13 in (1, 3))
-            assert(u14 in (2, 4))
-            assert(u16 == 0)
-            assert(u17 == 0)
-            assert(u18 == 0)
-
+            add_header(headers, ('undeciphered3 (DX11):' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
             decode_dx11_bind_info(file, num_sections, headers)
             assert(file.tell() - size - offset == 0)
+        elif shader_type == ShaderType.dx9:
+            undeciphered3.extend(consume_until_double_zero(file))
+
+            add_header(headers, ('undeciphered3 (DX 9):' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
+            decode_dx9_bind_info(file, headers)
+            assert(file.tell() - size - offset == 0)
         else:
-            assert(False)
+            assert(shader_type == ShaderType.opengl)
+            # OpenGL has nothing of interest here
 
         if headers:
             extract_unity_shaders.add_vanity_tag(headers)
