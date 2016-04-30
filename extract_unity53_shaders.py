5eefcbc731f04678b2177693f05e8306b33e286c
Ian Munsie <darkstarsword@gmail.com>
2016-04-30 15:30:43 +1000

    extract_unity53_shaders: Cleanups + consistent shader model header
    hlsltool looks for a header we need to add to determine the shader
    model, so print it in the format it expects. It isn't in the same
    position in the headers, but that shouldn't matter.

--- extract_unity53_shaders.py
+++ extract_unity53_shaders.py
@@ -4,30 +4,31 @@ import sys, os, argparse, glob, struct, zlib
 import extract_unity_shaders
 
 shader_type_mapping = {
-     1: (None, "OpenGL version 120"),
-     4: (None, "OpenGL version 300 es"),
-     5: (None, "OpenGL version 100"),
-     6: (None, "OpenGL version 150"),
+     1: (None, "opengl", "version 120"),
+     4: (None, "opengl", "version 300 es"),
+     5: (None, "opengl", "version 100"),
+     6: (None, "opengl", "version 150"),
 
-     9: ("vp", "DirectX9 vs_2_0"),
-    10: ("vp", "DirectX9 vs_3_0"),
-    11: ("fp", "DirectX9 ps_2_0"),
-    12: ("fp", "DirectX9 ps_3_0"),
+     9: ("vp", "d3d9", "vs_2_0"),
+    10: ("vp", "d3d9", "vs_3_0"),
+    11: ("fp", "d3d9", "ps_2_0"),
+    12: ("fp", "d3d9", "ps_3_0"),
 
-    13: ("vp", "DirectX11 (Feature Level 9) vs_4_0 + vs_2_0"),
-    14: ("fp", "DirectX11 (Feature Level 9) ps_4_0 + ps_2_0"),
+    13: ("vp", "d3d11_9x", "vs_4_0_level_9_1"),
+    14: ("fp", "d3d11_9x", "ps_4_0_level_9_1"),
 
-    15: ("vp", "DirectX11 vs_4_0"),
-    16: ("vp", "DirectX11 vs_5_0"),
-    17: ("fp", "DirectX11 ps_4_0"),
-    18: ("fp", "DirectX11 ps_5_0"),
+    15: ("vp", "d3d11", "vs_4_0"),
+    16: ("vp", "d3d11", "vs_5_0"),
+    17: ("fp", "d3d11", "ps_4_0"),
+    18: ("fp", "d3d11", "ps_5_0"),
 }
-
-def get_shader_type_name(shader_type):
-    return shader_type_mapping[shader_type][1]
 
 def get_program_name(shader_type):
     return shader_type_mapping[shader_type][0]
+def get_shader_api(shader_type):
+    return shader_type_mapping[shader_type][1]
+def get_shader_model(shader_type):
+    return shader_type_mapping[shader_type][2]
 
 class ParseError(Exception): pass
 
@@ -60,7 +61,7 @@ def consume_until_dx11_num_sections(file, undeciphered3):
 
 def add_header(headers, header):
     headers.append(header)
-    print(header)
+    # print(header)
 
 def decode_consts(file, headers, shader_type):
     (num_entries,) = struct.unpack('<I', file.read(4))
@@ -145,16 +146,24 @@ def decode_dx9_bind_info(file, headers):
     decode_binds(file, headers)
 
 def decode_dx11_bind_info(file, num_sections, headers):
-    print('num dx11 sections: {0}'.format(num_sections))
+    print('num dx11 bind info sections: {0}'.format(num_sections))
     decode_constbuffers(file, num_sections-1, headers, ShaderType.dx11)
     decode_binds(file, headers)
+
+def dump_raw_bind_info(file, dest, section_offset, section_size):
+    return # Remove to enable debugging dump
+
+    bind_info_size = section_size - (file.tell() - section_offset)
+    pos = file.tell()
+    with open(dest + '.rem', 'wb') as out:
+        out.write(file.read(bind_info_size))
+    file.seek(pos)
 
 def save_external_headers(dest, headers):
     extract_unity_shaders.add_vanity_tag(headers)
     headers = extract_unity_shaders.commentify(headers)
     with open('%s_headers.txt' % dest, 'w') as f:
         f.write(headers)
-
 
 def extract_opengl_shader(file, shader_size, headers, shader_name):
     shader = file.read(shader_size)
@@ -191,12 +200,7 @@ def extract_directx9_shader(file, shader_size, headers, section_offset, section_
     with open('%s.bin' % dest, 'wb') as out:
         out.write(shader)
 
-    if True: # Useful for debugging
-        bind_info_size = section_size - (file.tell() - section_offset)
-        pos = file.tell()
-        with open(dest + '.rem', 'wb') as out:
-            out.write(file.read(bind_info_size))
-        file.seek(pos)
+    dump_raw_bind_info(file, dest, section_offset, section_size)
 
     # Have not fully deciphered the data around this point, and depending
     # on the values the size can vary. Making a best guess based on
@@ -205,7 +209,7 @@ def extract_directx9_shader(file, shader_size, headers, section_offset, section_
     # versions, but can't make heads or tails of them.
     undeciphered3 = list(struct.unpack('<I', file.read(4)))
     undeciphered3.extend(consume_until_double_zero(file))
-    add_header(headers, ('undeciphered3 (DX 9):' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
+    add_header(headers, ('undeciphered3:' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
 
     decode_dx9_bind_info(file, headers)
     assert(file.tell() - section_size - section_offset == 0)
@@ -219,9 +223,6 @@ def extract_directx11_shader(file, shader_size, headers, section_offset, section
     shader = file.read(shader_size - 5)
     align(file, 4)
     assert(shader[:4] == b'DXBC')
-    assert(u8c >= 0)
-    assert(u8d >= 0)
-    assert(u8e >= 0)
 
     hash = extract_unity_shaders.fnv_3Dmigoto_shader(shader)
     path_components = extract_unity_shaders._export_filename_combined_short(
@@ -232,12 +233,7 @@ def extract_directx11_shader(file, shader_size, headers, section_offset, section
     with open('%s.bin' % dest, 'wb') as out:
         out.write(shader)
 
-    if True: # Useful for debugging
-        bind_info_size = section_size - (file.tell() - section_offset)
-        pos = file.tell()
-        with open(dest + '.rem', 'wb') as out:
-            out.write(file.read(bind_info_size))
-        file.seek(pos)
+    dump_raw_bind_info(file, dest, section_offset, section_size)
 
     # Have not fully deciphered the data around this point, and depending
     # on the values the size can vary. Making a best guess based on
@@ -250,7 +246,7 @@ def extract_directx11_shader(file, shader_size, headers, section_offset, section
         undeciphered3.extend(struct.unpack('<2I', file.read(8)))
 
     num_sections = consume_until_dx11_num_sections(file, undeciphered3)
-    add_header(headers, ('undeciphered3 (DX11):' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
+    add_header(headers, ('undeciphered3:' + ' {:x}'*len(undeciphered3)).format(*undeciphered3))
 
     decode_dx11_bind_info(file, num_sections, headers)
     assert(file.tell() - section_size - section_offset == 0)
@@ -264,7 +260,9 @@ def extract_shader_at(file, offset, size, shader_name):
     try:
         (u1, shader_type, u3, u4, u5, num_keywords) = struct.unpack('<6i', file.read(24))
         assert(u1 == 0x0c02c8a6)
-        add_header(headers, 'shader_type: {}'.format(get_shader_type_name(shader_type)))
+        api = get_shader_api(shader_type)
+        add_header(headers, 'API {}'.format(api))
+        add_header(headers, 'Shader model {}'.format(get_shader_model(shader_type)))
         add_header(headers, 'undeciphered1: {} {} {}'.format(u3, u4, u5))
 
         keywords = []
@@ -277,11 +275,11 @@ def extract_shader_at(file, offset, size, shader_name):
 
         (shader_size,) = struct.unpack('<i', file.read(4))
 
-        if shader_type in (1, 4, 5, 6):
+        if api == 'opengl':
             extract_opengl_shader(file, shader_size, headers, shader_name)
-        elif shader_type in (9, 10, 11, 12):
+        elif api == 'd3d9':
             extract_directx9_shader(file, shader_size, headers, offset, size, shader_name, get_program_name(shader_type))
-        elif shader_type in (13, 14, 15, 16, 17, 18):
+        elif api in ('d3d11', 'd3d11_9x'):
             extract_directx11_shader(file, shader_size, headers, offset, size, shader_name, get_program_name(shader_type))
         else:
             raise ParseError('Unknown shader type %i' % shader_type)
