5d20dff6ae485a2b6abc9585ce7ba84b33027a32
Ian Munsie <darkstarsword@gmail.com>
2016-06-20 07:20:33 +1000

    Dreamfall Chapters: Fix wildcards in autofix.sh
    The unity 5.3 shader extraction script stores the .bin files, which the
    wildcard in the autofix script matches and shadertool bails out. Add
    .txt to each wildcard in the autofix script so it only matches the
    correct files.

--- Dreamfall Chapters/autofix.sh
+++ Dreamfall Chapters/autofix.sh
@@ -11,7 +11,7 @@ cd ShaderCRCs
 cleanup_unity_shaders.py ../..
 
 # Order from most specific targets to least specific targets
-shadertool.py -I ../.. --disable=0 --condition=c200.x Hidden_Coord/*/* Hidden_Depth/*/* Hidden_DepthBreaks/*/* Hidden_Final\ Interpolation/*/* Hidden_InterpolateAlongRays/*/* Hidden_Raymarch/*/*
-shadertool.py -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/* Hidden_Internal-PrePass*/fp/* Hidden_ShadowSoftener-*/fp/* | unix2dos | tee -a ../../DX9Settings.ini
+shadertool.py -I ../.. --disable=0 --condition=c200.x Hidden_Coord/*/*.txt Hidden_Depth/*/*.txt Hidden_DepthBreaks/*/*.txt Hidden_Final\ Interpolation/*/*.txt Hidden_InterpolateAlongRays/*/*.txt Hidden_Raymarch/*/*.txt
+shadertool.py -I ../.. --fix-unity-lighting-ps-world --only-autofixed Hidden_Internal-Deferred*/fp/*.txt Hidden_Internal-PrePass*/fp/*.txt Hidden_ShadowSoftener-*/fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
 shadertool.py -I ../.. --auto-fix-vertex-halo --fix-unity-reflection --add-fog-on-sm3-update --only-autofixed --ignore-register-errors */vp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
 shadertool.py -I ../.. --fix-unity-reflection --only-autofixed --ignore-register-errors */fp/*.txt | unix2dos | tee -a ../../DX9Settings.ini
