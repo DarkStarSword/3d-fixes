8b42f67027ff7b2662f68d679ff6f18cfe18cc0a
Ian Munsie <darkstarsword@gmail.com>
2016-07-08 06:41:01 +1000

    Add 3dvisionlive.js to embed screenshots with multiple viewing methods
    Modern browsers are killing the nvidia plugin required for
    3dvisionlive.com - but we still want to use their storage. They have
    also never officially supported the cross-eyed or distance viewing
    methods.
    
    This javascript solves both of these problems. It can be included in a
    blog post to easily include screenshots and allow for multiple viewing
    methods: The plugin, cross-eyed, distance and anaglyph. It uses a cookie
    to remember the viewers preference.
    
    Using the script is as easy as copying this into the post:
    
    <script src="https://darkstarsword.net/3dvisionlive.js"></script>
    <noscript><p>Please enable javascript to view screenshots.</p></noscript>
    <script>
    embed_3dvisionlive('543e7878d475feb00e000093');
    embed_3dvisionlive('543d3927d475febb5c000175');
    embed_3dvisionlive('543e8ba8d475fef4570001ac');
    embed_3dvisionlive('543e8c6ad475febb5c0001c1');
    </script>
    
    Just substitute the IDs for the appropriate images on 3dvisionlive.com.
    The script will automatically place them in a table appropriate to the
    selected viewing mode.
    
    Every time the script is imported it will create a new screenshot block
    that subsequent embed_3dvisionlive() calls will append to. This allows
    the script to work on the front page where multiple posts may be
    displayed and importing the script. Changing the viewing mode on any
    block will update all of them. It might also be useful for posts with
    screenshots that aren't all grouped together.

---
+++ 3dvisionlive.js
@@ -0,0 +1,139 @@
+var screenshot_width = 470;
+var screenshot_height = 289;
+var distance_multiplier = 0.80;
+var current_screenshot_list = [];
+var current_screenshot_table;
+var current_screenshot_cols;
+var current_screenshot_row;
+var view_mode_html_fn;
+
+function add_screenshot_to_table(id)
+{
+	if (current_screenshot_row.childNodes.length == current_screenshot_cols) {
+		current_screenshot_row = document.createElement('tr');
+		current_screenshot_table.appendChild(current_screenshot_row);
+	}
+	cell = document.createElement('td');
+	cell.innerHTML = view_mode_html_fn(id);
+	current_screenshot_row.appendChild(cell);
+}
+
+function create_screenshot_table(container, screenshot_list)
+{
+	container.innerHTML = "";
+	current_screenshot_table = document.createElement('table');
+	current_screenshot_table.style = 'margin-left: auto; margin-right: auto;';
+	current_screenshot_row = document.createElement('tr');
+	current_screenshot_table.appendChild(current_screenshot_row);
+	for (var i in screenshot_list)
+		add_screenshot_to_table(screenshot_list[i]);
+	container.appendChild(current_screenshot_table);
+}
+
+function create_current_screenshot_table(container, screenshot_list)
+{
+	var containers = document.getElementsByClassName('screenshot_block');
+	create_screenshot_table(containers[containers.length - 1], screenshot_lists[containers.length - 1]);
+}
+
+function update_all_screenshot_blocks(html_fn, cols)
+{
+	current_screenshot_cols = cols;
+	view_mode_html_fn = html_fn;
+	var containers = document.getElementsByClassName('screenshot_block');
+	for (var i = 0; i < containers.length; i++)
+		create_screenshot_table(containers[i], screenshot_lists[i]);
+}
+
+function save_view_mode(mode)
+{
+	var d = new Date();
+	d.setTime(d.getTime() + 30*24*60*60*1000);
+	document.cookie = 'screenshot_view_mode=' + mode + '; expires=' + d.toGMTString() + '; path=/';
+}
+
+function view_mode_plugin()
+{
+	save_view_mode('plugin');
+	update_all_screenshot_blocks(function(id) {
+		return '<iframe src="http://photos.3dvisionlive.com/e/embed/' + id + '/nvidia/' + screenshot_width + '.' + screenshot_height + '/important" width="' + screenshot_width + '" height="' + screenshot_height + '" frameborder="1" vspace="0" hspace="0" marginwidth="0" marginheight="0" scrolling="no" noresize><p>See stereo 3D on  <a href="http://photos.3dvisionlive.com">photos.3dvisionlive.com</a></p></iframe>';
+	}, 2);
+}
+
+function view_mode_crosseyed()
+{
+	save_view_mode('crosseyed');
+	update_all_screenshot_blocks(function(id) {
+		return '<img src="http://api.photos.3dvisionlive.com/imagestore/' + id + '/nvidia./' + screenshot_width + '.' + screenshot_height + '/">';
+	}, 1);
+}
+
+function reverse_eyes(img, id)
+{
+	var canvas = document.getElementById('canvas_' + id);
+	var ctx = canvas.getContext('2d');
+	var w = img.naturalWidth / 2;
+	var h = img.naturalHeight;
+	canvas.width = w * 2;
+	canvas.height = h;
+
+	ctx.drawImage(img, w, 0, w, h, 0, 0, w, h);
+	ctx.drawImage(img, 0, 0, w, h, w, 0, w, h);
+}
+
+function view_mode_distance()
+{
+	save_view_mode('distance');
+	update_all_screenshot_blocks(function(id) {
+		var img = new Image();
+		img.onload = function() { reverse_eyes(this, id) };
+		img.id = 'screenshot_' + id;
+		var ratio = screenshot_width / screenshot_height * distance_multiplier;
+		img.src = 'http://api.photos.3dvisionlive.com/imagestore/' + id + '/nvidia./' + screenshot_width * distance_multiplier + '.' + screenshot_height * ratio + '/';
+		return '<div><canvas id="canvas_' + id + '"></canvas></div>';
+	}, 1);
+}
+
+function view_mode_anaglyph()
+{
+	save_view_mode('anaglyph');
+	update_all_screenshot_blocks(function(id) {
+		return '<img src="http://api.photos.3dvisionlive.com/imagestore/' + id + '/anaglyph./' + screenshot_width + '.' + screenshot_height + '/">';
+	} , 2);
+}
+
+function restore_view_mode() {
+	view_mode = document.cookie.match(new RegExp('(^| )screenshot_view_mode=([^;]+)'));
+	if (view_mode == null)
+		return view_mode_crosseyed();
+	switch (view_mode[2]) {
+	case 'plugin':
+		view_mode_plugin();
+		break;
+	case 'distance':
+		view_mode_distance();
+		break;
+	case 'anaglyph':
+		view_mode_anaglyph();
+		break;
+	default:
+		view_mode_crosseyed();
+	}
+}
+
+function embed_3dvisionlive(id)
+{
+	current_screenshot_list.push(id);
+	add_screenshot_to_table(id);
+}
+
+document.write('<p style="text-align: center;">Select viewing method: <a href="javascript:view_mode_plugin();">3D Vision Plugin</a> - <a href="javascript:view_mode_crosseyed();">Cross-eyed</a> - <a href="javascript:view_mode_distance();">Distance</a> - <a href="javascript:view_mode_anaglyph();">Anaglyph</a></p><div class="screenshot_block"></div>');
+
+// Check if the script has been called already on this page (blog front page shows multiple posts)
+if (typeof screenshot_lists === 'undefined') {
+	var screenshot_lists = [current_screenshot_list];
+	restore_view_mode();
+} else {
+	screenshot_lists.push(current_screenshot_list);
+	create_current_screenshot_table();
+}
