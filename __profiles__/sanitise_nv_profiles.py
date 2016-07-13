32fe97cd95be0444eb66a14d8db3c7c603180cd3
Ian Munsie <darkstarsword@gmail.com>
2016-07-13 11:59:44 +1000

    sanitise_nv_profiles: Properly decode internal settings
    This now properly decodes all the settings instead of just sanitising
    the encoding. The produced files are now back in utf16 since some of the
    strings do legitimately contain non-ascii characters.
    
    The internal settings (both strings and DWORDs) are XORed with a 2048
    bit key offset with a value derived from the setting ID. The key can be
    recovered using plain text attack or other methods, and the offsets are
    relatively straight forward to determine.

--- __profiles__/sanitise_nv_profiles.py
+++ __profiles__/sanitise_nv_profiles.py
@@ -6,43 +6,93 @@ wacko. This program cleans them up. Note that for now this process is one way.
 '''
 
 import sys, os, codecs
-import io
+import io, itertools, re, struct
 from xml.dom import minidom
 
-def process_string(string, o):
-	'''
-	"Strings" are just binary data embedded in the file, which disregards
-	the encoding of the rest of the file and must be handled specially.
-	Some of these are legitimate utf16 strings, so try that encoding first.
-	If that fails, convert them to a hex string instead.
-	'''
-	try:
-		o.write('"{}"'.format(string.decode('utf16')).encode('ascii'))
-	except:
-		# Requires a recent Python to use hex as a codec (or backport
-		# this to Python2)... IIRC this was added in Python 3.4:
-		o.write(b'0x' + codecs.encode(string, 'hex'))
+key = codecs.decode('2f7c4f8b2024528d263c9477f37c98a5fa71b680dd3584bafdb6a61b39c4ccb07e95d9ee184b9cf52d4ed0c15517df181e0b188b8858865a1e03ed56fb16fe8a01329c8df2e84ae6908e1568e82df440379a72c7020cd1d358ea62d198362bb216d5de93f1ba74e332c49ff612fe18c0bb35799c6b7a237f2b159b42071aff69fb9cbd2397a822638f32c8e99b631cee2cd9ed8d3a359cb160ae5ef5976b9f208cf7982c4379951dcd46366cd96720ab412221e55582f52720f508073f6d69d91c4bf826036eb23f1ee6ca3d6144b092aff088cae05f5df4dfc64ca4e0cab0205dc0fadd9a348f50795a5f7c199e407071b54519b853fcdf24be221c79bf4289', 'hex')
 
-def sanitise(in_filename, o):
-	i = open(in_filename, 'rb').read()
-
-	assert(i[0:2] == b'\xff\xfe')
-	pos = 2
-
+def readlines_utf16(i):
+	pos = 0
 	while True:
-		strpos = i.find(b'"\0', pos)
-		if strpos == -1:
-			o.write(i[pos:].decode('utf16').encode('ascii'))
-			break
+		eol = i.find(b'\n\0', pos)
+		if eol == -1:
+			yield i[pos:]
+			return
+		yield i[pos:eol + 2]
+		pos = eol + 2
 
-		o.write(i[pos:strpos].decode('utf16').encode('ascii'))
+def utf16(s):
+	r = s.encode('utf16')
+	if r[:2] == b'\xff\xfe':
+		return  r[2:]
+	return r
 
-		strendpos = i.find(b'"\0', strpos + 2)
+# I think I'm hitting a bug in the regexp if the string contains an extra " in the middle?
+# internal_string_pattern = re.compile(
+# 	utf16('SettingString ID_0x') +
+# 	b'(?P<SettingID>[0-9a-fA-F\0]{16})' +
+# 	utf16(' = "') +
+# 	b'(?P<String>.*)' +
+# 	utf16('" InternalSettingFlag=V0')
+# )
+# Breaking it up into two patterns solved the issue:
+internal_string_pattern1 = re.compile(
+	utf16('SettingString ID_0x') +
+	b'(?P<SettingID>[0-9a-fA-F\0]{16})' +
+	utf16(' = "')
+)
+internal_string_pattern2 = re.compile(
+	utf16('" InternalSettingFlag=V0')
+)
 
-		string = i[strpos + 2:strendpos]
-		process_string(string, o)
+def xor_strings(s1, s2):
+	return bytes([ c1 ^ c2 for c1, c2 in zip(s1, s2) ])
 
-		pos = strendpos + 2
+def decrypt_strings_utf16(i):
+	o = io.StringIO()
+	for line in readlines_utf16(i):
+		match1 = internal_string_pattern1.search(line)
+		match2 = internal_string_pattern2.search(line)
+		if match1 and match2:
+			id = int(match1.group('SettingID').decode('utf16'), 16)
+			string = line[match1.end():match2.start()]
+			off = (id << 1) % 256
+			k = itertools.cycle(itertools.chain(key[off:], key[:off]))
+			deciphered = xor_strings(string, k)
+			if deciphered[-2:] == b'\0\0':
+				deciphered = deciphered[:-2]
+			# print('Deciphered', hex(id), repr(deciphered.decode('utf16')))
+			o.write(line[:match1.end()].decode('utf16'))
+			o.write(deciphered.decode('utf16'))
+			o.write(line[match2.start():].decode('utf16'))
+		else:
+			o.write(line.decode('utf16'))
+
+	return o.getvalue()
+
+# I could merge this with the above if I wanted to do everything in one pass,
+# but the above routine is dealing with binary data, whereas doing this later
+# allows us to use the decoded strings, which is nicer. The key is used
+# slightly differently here as well, so cleaner to keep it separate.
+internal_dword_pattern = re.compile(r'''Setting ID_0x(?P<SettingID>[0-9a-fA-F]{8}) = 0x(?P<Value>[0-9a-fA-F]{8}) InternalSettingFlag=V0''')
+def decrypt_dwords(i):
+	o = io.StringIO()
+	for line in i.splitlines(True):
+		match = internal_dword_pattern.search(line)
+		if match:
+			id = int(match.group('SettingID'), 16)
+			span = match.span('Value')
+			value = int(line[span[0]:span[1]], 16)
+			off = (id << 1) % 256
+			k, = struct.unpack('<I', ((key[off:] + key[:off])[:4]))
+			deciphered = value ^ k
+			# print('Deciphered', hex(id), hex(value), '->', hex(deciphered))
+			o.write(line[:span[0]])
+			o.write('%08x' % deciphered)
+			o.write(line[span[1]:])
+		else:
+			o.write(line)
+	return o.getvalue()
 
 replace_map = []
 
@@ -102,14 +152,17 @@ def sort_profiles(data):
 
 def main():
 	parse_custom_setting_names_xml()
-	for file in sys.argv[1:]:
-		dest = '{}-cleaned.txt'.format(file[:file.rfind('.')])
-		stream = io.BytesIO() #FIXME: Refactor this away
-		sanitise(file, stream)
-		stream.seek(0)
-		buf = make_ids_friendly(stream.read().decode('ascii'))
+	for filename in sys.argv[1:]:
+		dest = '{}-cleaned.txt'.format(filename[:filename.rfind('.')])
+		i = open(filename, 'rb').read()
+
+		# The encrypted strings mess up the encoding, so decrypt them first and decode as utf16:
+		buf = decrypt_strings_utf16(i)
+		# Now the encoding has been cleanup up we can treat it as a regular string
+		buf = decrypt_dwords(buf)
+		buf = make_ids_friendly(buf)
 		buf = sort_profiles(buf)
-		open(dest, 'wb').write(buf.encode('ascii'))
+		open(dest, 'wb').write(buf.encode('utf16'))
 
 if __name__ == '__main__':
 	main()
