ccdd7b38ad54568141215077b6c91b4563a7203b
Ian Munsie <darkstarsword@gmail.com>
2016-07-14 18:42:55 +1000

    cleaup_unity_shaders: Add --remove-ini-sections-for-uninstalled-shaders option
    This option will remove ini sections for any shader that is not
    installed in ShaderFixes/ShaderOverride. It is intended to be used if
    the shaders are to be regenerated and their sections need to be
    regenerated as well.
    
    It might also be useful to cleanup ini sections for shaders in non-Unity
    games.
    
    Take caution - unlike Helix Mod, 3DMigoto does not require a shader to
    be installed in order to add a section to the ini for it, and this
    option would remove all such sections.

--- cleanup_unity_shaders.py
+++ cleanup_unity_shaders.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
 import sys, os, glob
-import subprocess, re
+import subprocess, re, argparse
 
 import shadertool
 
 section_pattern=re.compile('^\[(.*)\]')
-def cleanup_dx9_ini(path, ini_prefix, removed_hashes):
-	sections = set([ '[%s%s]' % (ini_prefix, os.path.splitext(x)[0]) for x in removed_hashes ])
+def cleanup_dx9_ini(path, ini_prefix, removed_basenames, preserve_basenames):
+	if removed_basenames is not None:
+		remove_sections = set([ '[%s%s]' % (ini_prefix, os.path.splitext(x)[0]) for x in removed_basenames ])
+	if preserve_basenames is not None:
+		preserve_sections = set([ '[%s%s]' % (ini_prefix, os.path.splitext(x)[0]) for x in preserve_basenames ])
 	lines = open(path, newline='\n').readlines()
+
+	def remove_section(section):
+		if removed_basenames is not None:
+			return section in remove_sections
+		assert(preserve_basenames is not None)
+		return section is not None and \
+			section.startswith('[' + ini_prefix) and \
+			section not in preserve_sections
 
 	current_section = None
 	last_line = 0
@@ -19,7 +30,7 @@ def cleanup_dx9_ini(path, ini_prefix, removed_hashes):
 		match = section_pattern.match(line)
 		if match:
 			last_was_blank = False
-			if current_section in sections:
+			if remove_section(current_section):
 				output.extend(section[last_line:])
 				current_section = match.group(0)
 				section = [line]
@@ -40,18 +51,28 @@ def cleanup_dx9_ini(path, ini_prefix, removed_hashes):
 			else:
 				last_was_blank = False
 
-	if current_section not in sections:
-		output.extend(section)
-	else:
+	if remove_section(current_section):
 		output.extend(section[last_line:])
+	else:
+		output.extend(section)
 
 	open(path, 'w', newline='\n').write(''.join(output))
 
+# TODO: Refactor with above function
 shaderoverride_pattern = re.compile('^\[ShaderOverride.*\]', re.IGNORECASE)
 hash_pattern = re.compile(r'^\s*hash\s*=\s*([0-9a-f]+)\s*$', re.IGNORECASE)
-def cleanup_dx11_ini(path, removed_hashes):
-	hashes = set([ x.split('-', 1)[0] for x in removed_hashes ])
+def cleanup_dx11_ini(path, removed_basenames, preserve_basenames):
+	if removed_basenames is not None:
+		remove_hashes = set([ x.split('-', 1)[0] for x in removed_basenames ])
+	if preserve_basenames is not None:
+		preserve_hashes = set([ x.split('-', 1)[0] for x in preserve_basenames ])
 	lines = open(path, newline='\n').readlines()
+
+	def remove_hash(hash):
+		if removed_basenames is not None:
+			return hash in remove_hashes
+		assert(preserve_basenames is not None)
+		return is_shaderoverride_section and hash not in preserve_hashes
 
 	is_shaderoverride_section = False
 	current_shaderoverride_hash = None
@@ -62,14 +83,15 @@ def cleanup_dx11_ini(path, removed_hashes):
 	for line in lines:
 		if section_pattern.match(line):
 			last_was_blank = False
-			is_shaderoverride_section = not not shaderoverride_pattern.match(line)
-			if current_shaderoverride_hash in hashes:
+			if remove_hash(current_shaderoverride_hash):
 				output.extend(section[last_line:])
+				is_shaderoverride_section = not not shaderoverride_pattern.match(line)
 				current_shaderoverride_hash = None
 				section = [line]
 				last_line = 0
 				continue
 			output.extend(section)
+			is_shaderoverride_section = not not shaderoverride_pattern.match(line)
 			section = [line]
 			current_shaderoverride_hash = None
 			last_line = 0
@@ -88,24 +110,39 @@ def cleanup_dx11_ini(path, removed_hashes):
 			else:
 				last_was_blank = False
 
-	if current_shaderoverride_hash not in hashes:
-		output.extend(section)
-	else:
+	if remove_hash(current_shaderoverride_hash):
 		output.extend(section[last_line:])
+	else:
+		output.extend(section)
 
 	open(path, 'w', newline='\n').write(''.join(output))
 
-def cleanup_common(game_dir, git_path, extracted_glob, shader_path, ini_filename, cleanup_ini):
+def cleanup_ini_from_installed_only(game_dir, git_path, profile):
+	tmp = glob.glob(os.path.join(git_path, profile.shader_path, '*.txt'))
+	installed_basenames = set([ os.path.basename(x) for x in tmp ])
+
+	ini_path = os.path.join(git_path, profile.ini_filename)
+	profile.cleanup_ini(ini_path, None, installed_basenames)
+
+	command = ['git', '-C', git_path, 'add', profile.ini_filename]
+	print("Running '%s'..." % ' '.join(command))
+	subprocess.call(command)
+
+	profile.cleanup_ini(os.path.join(game_dir, profile.ini_filename), None, installed_basenames)
+
+def cleanup(game_dir, git_path, profile):
+	if args.remove_ini_sections_for_uninstalled_shaders:
+		return cleanup_ini_from_installed_only(game_dir, git_path, profile)
+
 	# TODO: Make case insensitive
+	tmp = glob.glob(os.path.join(game_dir, profile.extracted_glob))
+	extracted_basenames = set([ os.path.basename(x) for x in tmp ])
 
-	tmp = glob.glob(os.path.join(game_dir, extracted_glob))
-	hashes = set([ os.path.basename(x) for x in tmp ])
+	tmp = glob.glob(os.path.join(git_path, profile.shader_path, '*.txt'))
+	installed_basenames = set([ os.path.basename(x) for x in tmp ])
 
-	tmp = glob.glob(os.path.join(git_path, shader_path, '*.txt'))
-	installed = set([ os.path.basename(x) for x in tmp ])
-
-	removed_hashes = installed.difference(hashes)
-	removed_files = [ os.path.join(shader_path, x) for x in removed_hashes ]
+	removed_basenames = installed_basenames.difference(extracted_basenames)
+	removed_files = [ os.path.join(profile.shader_path, x) for x in removed_basenames ]
 
 	if not removed_files:
 		return
@@ -114,51 +151,62 @@ def cleanup_common(game_dir, git_path, extracted_glob, shader_path, ini_filename
 	print("Running '%s'..." % ' '.join(command))
 	subprocess.call(command)
 
-	ini_path = os.path.join(git_path, ini_filename)
-	cleanup_ini(ini_path, removed_hashes)
+	ini_path = os.path.join(git_path, profile.ini_filename)
+	profile.cleanup_ini(ini_path, removed_basenames, None)
 
-	command = ['git', '-C', git_path, 'add', ini_filename]
+	command = ['git', '-C', git_path, 'add', profile.ini_filename]
 	print("Running '%s'..." % ' '.join(command))
 	subprocess.call(command)
 
-	for file in removed_hashes:
-		path = os.path.join(game_dir, shader_path, file)
+	for file in removed_basenames:
+		path = os.path.join(game_dir, profile.shader_path, file)
 		print("rm '%s'" % path)
 		try:
 			os.remove(path)
 		except FileNotFoundError:
 			pass
 
-	cleanup_ini(os.path.join(game_dir, ini_filename), removed_hashes)
+	profile.cleanup_ini(os.path.join(game_dir, profile.ini_filename), removed_basenames, None)
 
-def cleanup_dx9(game_dir, git_path, unity_type, ini_prefix, helix_type):
-	extracted_glob = 'extracted/ShaderCRCs/*/%s/*.txt' % unity_type
-	shader_path = os.path.join('ShaderOverride', helix_type)
-	ini_filename = 'DX9Settings.ini'
-	cleanup_ini = lambda path, removed_hashes : cleanup_dx9_ini(path, ini_prefix, removed_hashes)
-	return cleanup_common(game_dir, git_path, extracted_glob, shader_path, ini_filename, cleanup_ini)
+class CleanupDX9(object):
+	def __init__(self, unity_type, ini_prefix, helix_type):
+		self.extracted_glob = 'extracted/ShaderCRCs/*/%s/*.txt' % unity_type
+		self.shader_path = os.path.join('ShaderOverride', helix_type)
+		self.ini_filename = 'DX9Settings.ini'
+		self.cleanup_ini = lambda path, removed_basenames, preserve_basenames : cleanup_dx9_ini(path, ini_prefix, removed_basenames, preserve_basenames)
 
-def cleanup_dx11(game_dir, git_path):
-	extracted_glob = 'extracted/ShaderFNVs/*/*.txt'
-	shader_path = 'ShaderFixes'
-	ini_filename = 'd3dx.ini'
-	cleanup_ini = lambda path, removed_hashes : cleanup_dx11_ini(path, removed_hashes)
-	return cleanup_common(game_dir, git_path, extracted_glob, shader_path, ini_filename, cleanup_ini)
+class CleanupDX11(object):
+	def __init__(self):
+		self.extracted_glob = 'extracted/ShaderFNVs/*/*.txt'
+		self.shader_path = 'ShaderFixes'
+		self.ini_filename = 'd3dx.ini'
+		self.cleanup_ini = lambda path, removed_basenames, preserve_basenames : cleanup_dx11_ini(path, removed_basenames, preserve_basenames)
+
+def parse_args():
+	global args
+	parser = argparse.ArgumentParser(description = 'Stale Shader Removal Tool')
+	parser.add_argument('games', nargs='+',
+		help='List of game directories to process')
+	parser.add_argument('--remove-ini-sections-for-uninstalled-shaders', action='store_true',
+		help='Remove any ini sections for shaders not found in ShaderFixes/ShaderOverride. CAUTION: 3DMigoto does not require shaders to be installed to use with ini sections - this option will remove these!')
+	args = parser.parse_args()
 
 def main():
-	for game_dir in map(os.path.realpath, sys.argv[1:]):
+	parse_args()
+	for game_dir in map(os.path.realpath, args.games):
 		git_path = shadertool.game_git_dir(game_dir)
 		found = False
 		if os.path.exists(os.path.join(git_path, 'DX9Settings.ini')):
-			cleanup_dx9(game_dir, git_path, 'vp', 'VS', 'VertexShaders')
-			cleanup_dx9(game_dir, git_path, 'fp', 'PS', 'PixelShaders')
+			cleanup(game_dir, git_path, CleanupDX9('vp', 'VS', 'VertexShaders'))
+			cleanup(game_dir, git_path, CleanupDX9('fp', 'PS', 'PixelShaders'))
 			found = True
 		if os.path.exists(os.path.join(git_path, 'd3dx.ini')):
-			cleanup_dx11(game_dir, git_path)
+			cleanup(game_dir, git_path, CleanupDX11())
 			found = True
 
 		if found:
-			command = [ 'git', '-C', git_path, 'commit', '-m', '%s: run cleanup_unity_shaders.py' % os.path.basename(git_path) ]
+			command = [ 'git', '-C', git_path, 'commit', '-m', '%s: run %s' %
+					(os.path.basename(git_path), ' '.join([os.path.basename(sys.argv[0])] + sys.argv[1:])) ]
 			print("Running '%s'..." % ' '.join(command))
 			subprocess.call(command)
 		else:
