4bd6ae0519cc4e94c87ea6d2e5d3865c57eda8be
Ian Munsie <darkstarsword@gmail.com>
2016-11-06 17:00:29 +1100

    Unity 5.4 template / Firewatch: Fix new directional lighting shader
    Like the physical lighting shader this one has changed slightly and has
    a new hash. It is also worth noting that it has been renamed from
    "Internal-PrePassCollectShadows" to "Internal-ScreenSpaceShadows".

--- Unity5/DX11/autofix53.sh
+++ Unity5/DX11/autofix53.sh
@@ -50,6 +50,8 @@ if [ $FIX_LIGHTING -eq 1 ]; then
 	echo "Applying directional lighting fix..."
 	find . \( -name 'bfae1ae6908d87a2-vs*' \
 	       -o -name 'f51c2a7085326040-vs*' \
+	       -o -name 'f1dfaa0a76663bf9-vs*' \
+	       -o -name 'bc1b4298b3713fce-vs*' \
 	       \) -a -print0 | xargs -0 dirname -z | sort -uz | sed -z 's/$/\/*-ps_replace.txt/' | xargs -0 \
 			hlsltool.py -I ../.. --fix-unity-lighting-ps=TEXCOORD4 --only-autofixed --fxc "$FXC" $LIGHTING_EXTRA | update_ini
 fi
