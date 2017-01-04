2eb69670b61253650be8fb776f54fb70eaeee1c3
Ian Munsie <darkstarsword@gmail.com>
2017-01-04 15:56:32 +1100

    WATCH_DOGS2: Note differences in fog output, begin autofix.sh
    Take note of what UAVs are written from each of the fog compute shaders
    to try to understand which shader will use which adjustment, and start
    an autofix script to apply them.

---
+++ WATCH_DOGS2/autofix.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+DIR=~/3d-fixes
+PATH="$DIR:$PATH"
+
+cs=$(ls *cs.txt)
+ps=$(ls *ps.txt)
+
+# Filter out all confetti physics compute shaders - these must have identical
+# inputs and execute the same instructions on both eyes otherwise physics will
+# desync:
+cs=$(grep -L Confetti $cs)
+
+# Sun/moon fog shaders - these need to be moved to infinity:
+sun_moon_fog=$(grep -l FillVolumeTexture__VFOutputBuffer4 $cs)
+cs=$(grep -L FillVolumeTexture__VFOutputBuffer4 $cs)
+asmtool.py --fix-wd2-view-dir-reconstruction -i -f --only-autofixed $sun_moon_fog
+
+# Fog fix alternate 1 (Adjusts all CS fog except sun/moon. Includes density):
+fog_shaders=$(grep -l 'cbuffer VolumetricFog' $cs)
+cs=$(grep -L 'cbuffer VolumetricFog' $cs)
+asmtool.py --fix-wd2-volumetric-fog -i -f --only-autofixed $fog_shaders
+
+# Fog fix alternate 2 (Adjusts all CS & PS fog except sun/moon and density. Seems less accurate in some cases?):
+# asmtool.py --fix-wd2-unproject --fix-wd2-camera-pos --fix-wd2-view-dir-reconstruction --fix-wd2-camera-z-axis --fix-wd2-screen-space-reflections --fix-wd2-screen-space-reflections-cs -i -f --only-autofixed $cs
+
+# Apply alternate 1 fog fix only to FV ("Fog Volume" as opposed to "Volumetric
+# Fog" o_O ?) pixel shaders:
+fv_shaders_ps=$(grep -l 'cbuffer FogVolumeRaymarch' $ps)
+ps=$(grep -L 'cbuffer FogVolumeRaymarch' $ps)
+asmtool.py --fix-wd2-volumetric-fog -i -f --only-autofixed $fv_shaders_ps
+
+# Apply alternate 2 fog fix to *all* pixel shaders that mention fog (alternate
+# 1 fix completely breaks windshield reflections, while this breaks some fog):
+#fog_shaders_ps=$(grep -l 'cbuffer VolumetricFog' $ps)
+#asmtool.py --fix-wd2-unproject --fix-wd2-camera-pos --fix-wd2-view-dir-reconstruction --fix-wd2-camera-z-axis --fix-wd2-screen-space-reflections --fix-wd2-screen-space-reflections-cs -i -f --only-autofixed $fog_shaders_ps
