a65e7e7be41b94c43be6071d608e440a5a21417f
Ian Munsie <darkstarsword@gmail.com>
2017-01-22 17:15:05 +1100

    Add script to convert images taken with the Loreo 9005 lens to .jps
    Script to convert photos taken with the Loreo 9005 3D DSLR lens in a cap
    into .jps format. This script is not intended to be used alone - the
    .jps files should be cropped with my Stereo Photo Cropping Tool
    afterwards to adjust the parallax and/or crop off the overlap. By
    default this tool will copy 5% overlap from the center of the image to
    both eyes - the amount can be adjusted, but keep in mind that since the
    divider between eyes is not in a fixed position on this lens (and there
    is no way to automatically determine what it was set to), you should be
    generous.

---
+++ photo-gallery/loreo2jps.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Script to convert photos taken with the Loreo 9005 3D DSLR lens in a cap into
+# .jps format. This script is not intended to be used alone - the .jps files
+# should be cropped with my Stereo Photo Cropping Tool afterwards to adjust the
+# parallax and/or crop off the overlap. By default this tool will copy 5%
+# overlap from the center of the image to both eyes - the amount can be
+# adjusted below, but keep in mind that since the divider between eyes is not
+# in a fixed position on this lens (and there is no way to automatically
+# determine what it was set to), you should be generous.
+
+# OTOH for convenience you may want to set this negative to remove the overlap
+# from both eyes, or 0 to just switch eyes... However, if you aren't going to
+# do this properly you should probably not even bother swapping eyes and just
+# leave the image as something that can be viewed with the distance method or
+# leave it to the viewer to swap eyes - i.e. this is the wrong tool for you.
+
+overlap=5
+
+for file in "$@"; do
+	dcraw -e "$file"
+	base=$(basename "$file" .CR2)
+	jpg="${base}.thumb.jpg"
+	jps="${base}.jps"
+	width=$(identify -format '%w' "$jpg")
+	height=$(identify -format '%h' "$jpg")
+	w2=$((($width / 2) + ($width * $overlap / 200)))
+	x2=$((($width / 2) - ($width * $overlap / 200)))
+	convert "$jpg[${w2}x${height}+${x2}+0]" "$jpg[${w2}x${height}+0+0]" +append "$jps"
+	rm "$jpg"
+done
