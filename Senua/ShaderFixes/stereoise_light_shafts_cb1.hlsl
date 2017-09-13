2ab09d53dd1bbc6137da52da23ec3aefb5812f39
Ian Munsie <darkstarsword@gmail.com>
2017-09-14 06:59:40 +1000

    Senua: Fix volumetric lighting / light shafts
    Well, this effect looks *very* familiar - I am quite certain that this
    is the exact same volumetric lighting used in Far Cry Primal and
    WATCH_DOGS2. Only difference here is that since this is UE4 the headers
    have been stripped so asmtool can't follow it, but no problem - let's
    use my new constant buffer stereoisation technique instead :)
    
    Since this isn't a standard UE4 effect it doesn't use the standard UE4
    constant buffer, so we have to analyse the constant buffers it is using
    and match the matrices and camera position up, then use custom shaders
    to copy the stereoised versions produced by my UE4 stereoisation shader.
    
    This fixes light shafts, fog volumes, etc. There might be more shaders
    to find (and if it's like WATCH_DOGS2 they may *not* all be compute
    shaders), but with no way to extract and match them up we will have to
    find them as we go.
    
    Note that it was *very* important that the transformation matrices from
    the previous frame be adjusted as well.

---
+++ Senua/ShaderFixes/stereoise_light_shafts_cb1.hlsl
@@ -0,0 +1,40 @@
+// DarkStarSword's Senua's Sacrifice Light Shaft Steroisation Shader
+
+#define NEW_UE4
+#define SENUA
+
+#include "UE4FViewUniformShaderParameters.hlsl"
+
+// Clearly not a stock UE4 effect - most of these are readily available in
+// FViewUniformShaderParameters, so they could have just used that directly.
+// Used Frame Analysis to match these up.
+
+struct LightShaftsCB1 {
+	/*   4 ->  0 */ FMatrix WorldToClip;
+	/*  44 ->  4 */ FMatrix ScreenToTranslatedWorld;
+	/*   8 ->  8 */ FMatrix TranslatedWorldToView;
+	/*  56 -> 12 */ FVector WorldCameraOrigin;
+	float UAV_PADDING_WorldCameraOrigin; // Has the value 10
+	/*  64 -> 13 */ FMatrix PrevViewProj;
+	/* 107 -> 17 */ FMatrix PrevScreenToTranslatedWorld;
+	/*  85 -> 21 */ FMatrix PrevTranslatedWorldToView;
+	/* 100 -> 25 */ FVector PrevWorldCameraOrigin;
+	float UAV_PADDING_PrevWorldCameraOrigin; // Has the value 10
+	// Effect specific effects follow
+};
+
+RWStructuredBuffer<struct LightShaftsCB1> light_shafts_cb1 : register(u0);
+StructuredBuffer<struct FViewUniformShaderParameters> stereo : register(t113);
+
+[numthreads(1, 1, 1)]
+void main(uint3 tid: SV_DispatchThreadID)
+{
+	light_shafts_cb1[0].WorldToClip = stereo[0].WorldToClip;
+	light_shafts_cb1[0].ScreenToTranslatedWorld = stereo[0].ScreenToTranslatedWorld;
+	light_shafts_cb1[0].TranslatedWorldToView = stereo[0].TranslatedWorldToView;
+	light_shafts_cb1[0].WorldCameraOrigin = stereo[0].WorldCameraOrigin;
+	light_shafts_cb1[0].PrevViewProj = stereo[0].PrevViewProj;
+	light_shafts_cb1[0].PrevScreenToTranslatedWorld = stereo[0].PrevScreenToTranslatedWorld;
+	light_shafts_cb1[0].PrevTranslatedWorldToView = stereo[0].PrevTranslatedWorldToView;
+	light_shafts_cb1[0].PrevWorldCameraOrigin = stereo[0].PrevWorldCameraOrigin;
+}
