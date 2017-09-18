49007cb90c386cbc9d34381dcf74441b1b9cbfe4
Ian Munsie <darkstarsword@gmail.com>
2017-09-18 10:17:02 +1000

    UE4/remap_cb.cpp: Add regular expression shadow fix
    Some of the UE4 shadow shaders use a per-draw ScreenToShadowMatrix.
    While we can use an inverse screen-space stereo injection matrix
    (e.g. derived from the stereo and mono versions of ScreenToWorld) to
    inject a stereo correction into that matrix, it is wasteful since it is
    only used for one draw call, and uncertain how reliably we can detect
    the buffer it is in since AFAICT it is just the shader's $Globals
    buffer.
    
    Instead, let's fall back to more traditional techniques for these
    shaders and perform a regular expression search and replace on their
    assembly to inject a stereo correction. For now the pattern is pretty
    precise, but that can be relaxed as we find more variants of these (we
    might even be able to get away with only matching the matrix multiply).
    
    This also gave me a chance to start playing around with the regex
    implementation in C++11, which barring any MS stupidity (have hit this
    last time I looked at regex in MSVC) we might be able to adapt to some
    form of more general autofix engine in 3DMigoto. If there is stupidity
    to be found I might end up pulling in something like PCRE...

--- UE4/remap_cb_cpp/remap_cb.cpp
+++ UE4/remap_cb_cpp/remap_cb.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <fstream>
 #include <stdio.h>
+#include <regex>
 
 #define LogInfo printf
 #define D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT 14
@@ -86,7 +87,7 @@ static bool patch_cb(std::string *asm_text, unsigned cb_reg, size_t *dcl_end, un
 	return true;
 }
 
-static bool patch_ue4_asm_text(std::string *asm_text,
+static bool patch_asm_redirect_cb(std::string *asm_text,
 		bool patch_cbuffers[D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT],
 		int disable_driver_stereo_vs_reg, int disable_driver_stereo_ds_reg)
 {
@@ -98,6 +99,7 @@ static bool patch_ue4_asm_text(std::string *asm_text,
 	std::string insert_str;
 	int disable_driver_stereo_reg = -1;
 
+	LogInfo("Analysing shader constant buffer usage...\n");
 	for (
 		shader_model_pos = asm_text->find("\n");
 		shader_model_pos != std::string::npos && (*asm_text)[shader_model_pos + 1] == '/';
@@ -152,7 +154,9 @@ static bool patch_ue4_asm_text(std::string *asm_text,
 		if (!patch_cbuffers[cb_reg])
 			continue;
 
-		patched = patch_cb(asm_text, cb_reg, &dcl_end, &tmp_regs, shader_model_major) || patched;
+		patch_cbuffers[cb_reg] = patch_cb(asm_text, cb_reg, &dcl_end, &tmp_regs, shader_model_major);
+
+		patched = patched || patch_cbuffers[cb_reg];
 	}
 
 	// XXX: Here or after disabling the driver stereo cb?
@@ -164,9 +168,13 @@ static bool patch_ue4_asm_text(std::string *asm_text,
 			+ std::to_string(disable_driver_stereo_reg)
 			+ std::string("[4], immediateIndexed");
 		LogInfo("Disabling driver stereo correction: %s\n", insert_str.substr(1).c_str());
+		insert_str = std::string("\n\n// Disables driver stereo correction:") + insert_str;
 		asm_text->insert(dcl_end, insert_str);
 		dcl_end += insert_str.size();
 	}
+
+	insert_str = std::string("\n\n// Constant buffers redirected by DarkStarSword's UE4 autofix:");
+	asm_text->insert(dcl_end, insert_str);
 
 	if (dcl_temps != std::string::npos) {
 		dcl_temps += 11;
@@ -185,12 +193,129 @@ static bool patch_ue4_asm_text(std::string *asm_text,
 	return true;
 }
 
+static std::string ue4_shadow_pattern(
+	R"(()" // $1
+		R"(ps_5_0\n)"
+		R"(dcl_globalFlags refactoringAllowed \| forceEarlyDepthStencil\n)"
+		R"(dcl_constantbuffer CB0\[32\], immediateIndexed\n)"
+		R"(dcl_constantbuffer CB1\[124\], immediateIndexed\n)"
+		R"(dcl_sampler s0, mode_default\n)"
+		R"(dcl_sampler s1, mode_default\n)"
+		R"(dcl_sampler s2, mode_default\n)"
+		R"(dcl_sampler s3, mode_default\n)"
+		R"(dcl_resource_texture2d \(float,float,float,float\) t0\n)"
+		R"(dcl_resource_texture2d \(float,float,float,float\) t1\n)"
+		R"(dcl_resource_texture2d \(float,float,float,float\) t2\n)"
+		R"(dcl_resource_texture2d \(float,float,float,float\) t3\n)"
+		R"(dcl_input_ps_siv linear noperspective v0\.xy, position\n)"
+		R"(dcl_output o0\.xyzw\n)"
+//		R"(dcl_temps \d+\n)"
+
+		R"(dcl_temps) (\d+)\n)" // $2
+
+	// Insert declarations here
+
+	// Alternatively, insert instructions to multiply an inverse
+	// screen-space stereo injection matrix by ScreenToShadowMatrix here
+
+	R"(()" // $3
+		R"(mul r0\.xy, v0\.xyxx, cb1\[123\]\.zwzz\n)"
+		R"(sample_l_indexable\(texture2d\)\(float,float,float,float\) r0\.z, r0\.xyxx, t2\.yzxw, s2, l\(0\.000000\)\n)"
+		R"(mad r0\.w, r0\.z, cb1\[53\]\.x, cb1\[53\]\.y\n)"
+		R"(mad r0\.z, r0\.z, cb1\[53\]\.z, -cb1\[53\]\.w\n)"
+		R"(div r0\.z, l\(1\.000000, 1\.000000, 1\.000000, 1\.000000\), r0\.z\n)"
+		R"(add r0\.z, r0\.z, r0\.w\n)"
+		R"(mad r1\.xy, v0\.xyxx, cb1\[123\]\.zwzz, -cb1\[54\]\.wzww\n)"
+		R"(div r1\.xy, r1\.xyxx, cb1\[54\]\.xyxx\n)"
+		R"(mul r1\.xy, r0\.zzzz, r1\.xyxx\n)"
+	R"())"
+
+	// Insert fix here
+
+	R"(()" // $4
+		R"(mul r2\.xyzw, r1\.yyyy, cb0\[28\]\.xyzw\n)"
+		R"(mad r1\.xyzw, r1\.xxxx, cb0\[27\]\.xyzw, r2\.xyzw\n)"
+		R"(mad r1\.xyzw, r0\.zzzz, cb0\[29\]\.xyzw, r1\.xyzw\n)"
+		R"(add r1\.xyzw, r1\.xyzw, cb0\[30\]\.xyzw\n)"
+		R"(div r\d+\.[xyzw]{2}, r1\.[xyzw]{4}, r1\.wwww\n)"
+	R"())"
+);
+
+// Ignore case for compatibility with d3dcompiler_46 & 47 without having to
+// always remember to account for the dcl_constantbuffer difference:
+static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::ECMAScript | std::regex::icase);
+
+// Other grammars, though really if we are thinking about grammars we should
+// pull in something that supports named groups like Python's re (Does PCRE?):
+//static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::basic | std::regex::icase);
+//static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::extended | std::regex::icase);
+//static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::awk | std::regex::icase);
+//static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::grep | std::regex::icase);
+//static std::regex ue4_shadow_regex(ue4_shadow_pattern, std::regex::egrep | std::regex::icase);
+
+static char ue4_shadow_replace[] =
+	"$1 {dcl_temps}\n"
+
+	//"\n// StereoParams automatically inserted by DarkStarSword's UE4 autofix:\n"
+        "dcl_resource_texture2d (float,float,float,float) t125\n"
+        "\n"
+
+	"$3"
+
+	"\n// Shadows automatically corrected by DarkStarSword's UE4 autofix:\n"
+	// NOTE: which ld instruction we use depends on the shader model.
+	// Since we only matched ps_5_0 we are fine to use ld_indexable:
+        "ld_indexable(texture2d)(float,float,float,float) r{stereo}.xyzw, l(0, 0, 0, 0), t125.xyzw\n"
+        "add r{stereo}.w, r0.z, -r{stereo}.y\n"
+        "mad r1.x, -r{stereo}.w, r{stereo}.x, r1.x\n"
+        "\n"
+
+	"$4"
+;
+
+// TODO: Use a simple string search and replace
+static std::regex regex_stereo(R"(\{stereo\})");
+static std::regex regex_dcl_temps(R"(\{dcl_temps\})");
+
+bool patch_ue4_shadows_regex(std::string *asm_text)
+{
+	unsigned tmp_regs = 0;
+	std::smatch match;
+	std::string replace;
+	std::smatch::difference_type pos, len;
+
+	// LogInfo("pattern:\n%s\n\n", ue4_shadow_pattern.c_str());
+
+	std::regex_search(*asm_text, match, ue4_shadow_regex);
+
+	if (!match.size())
+		return false;
+
+	LogInfo("Shader matches UE4 shadow pattern\n");
+	// LogInfo("matched:\n%s\n", match[0].str().c_str());
+
+	tmp_regs = stoul(match[2]);
+	LogInfo("Found dcl_temps %d\n", tmp_regs);
+
+	replace = std::regex_replace(ue4_shadow_replace, regex_stereo, std::to_string(tmp_regs++));
+	replace = std::regex_replace(replace, regex_dcl_temps, std::to_string(tmp_regs));
+
+	// LogInfo("\nreplace string:\n%s\n", replace.c_str());
+
+	pos = match.position();
+	len = match.length();
+
+	asm_text->replace(pos, len, std::regex_replace(match.str(), ue4_shadow_regex, replace));
+
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
 	bool patch_cbuffers [D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT] = {false};
 	std::string shader;
 	unsigned i, j;
-	bool patched;
+	bool patched = false;
 
 	if (argc < 2) {
 		printf("Usage: %s file.asm\n", argv[0]);
@@ -200,6 +325,8 @@ int main(int argc, char *argv[])
 	std::ifstream ifs(argv[1]);
 	shader.assign((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
 
+	patched = patch_ue4_shadows_regex(&shader);
+
 	for (i = 2; i < argc; i++) {
 		j = atol(argv[i]);
 		if (j >= D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT)
@@ -207,7 +334,8 @@ int main(int argc, char *argv[])
 		patch_cbuffers[j] = true;
 	}
 
-	patched = patch_ue4_asm_text(&shader, patch_cbuffers, 12, 12);
+	if (argc > 2)
+		patched = patch_asm_redirect_cb(&shader, patch_cbuffers, 12, 12) || patched;
 
 	if (patched)
 		printf("\nPatched shader:\n%s", shader.c_str());
