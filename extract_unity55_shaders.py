1a51aa94b137c2801fd2b189bbc3b4b3e96fe205
Ian Munsie <darkstarsword@gmail.com>
2017-12-22 23:42:05 +1100

    extract_unity55_shaders: Small progress deciphering next data structure
    This next data structure looks likely to be encoding the subshader
    sections from the old .shader files. This is going to be tricky - some
    patterns are emerging, but the overall structure including how to
    determine entry lengths is still unclear. This decodes the tags
    (uncertain if they are subshader or pass tags) if present, but little
    else - I'll need more shaders to analyse this, and preferably shaders
    where I still have an old .shader file to compare.

--- extract_unity55_shaders.py
+++ extract_unity55_shaders.py
@@ -10,18 +10,22 @@ def parse_string(file):
     align(file, 4)
     return string
 
-def parse_unknown_tex_table(file):
+def parse_properties_table(file):
     (table_len,) = struct.unpack('<I', file.read(4))
+    if table_len == 0:
+        return
 
-    print('Unknown texture table length: %i' % table_len)
+    print('Properties {')
     for i in range(table_len):
         name = parse_string(file)
         desc = parse_string(file)
         (u1, u2, u3, r, g, b, a) = struct.unpack('<3I4f', file.read(7*4))
         color = parse_string(file)
         (u4,) = struct.unpack('<I', file.read(4))
+        # FIXME: Match the formatting of the old .shader files
         print('  {name} "{desc}" {u1:x} {u2:x} {u3:x} {r} {g} {b} {a} "{color}" {u4}'.format(
             name=name, desc=desc, u1=u1, u2=u2, u3=u3, r=r, g=g, b=b, a=a, color=color, u4=u4))
+    print('}')
 
 def parse_keywords_and_bindings_table(file):
     (num_keywords_and_bindings,) = struct.unpack('<I', file.read(4))
@@ -79,14 +83,65 @@ def parse_unknown_init_table(file):
         parse_unknown_init_float_entry(file, i)
     parse_unknown_init_color_entry(file, 84)
 
+def hexdump(buf, start=0, width=16, indent=0):
+	a = ''
+	for i, b in enumerate(buf):
+		if i % width == 0:
+			if i:
+				print(' | %s |' % a)
+			print('%s  %08x: ' % (' ' * indent, start + i), end='')
+			a = ''
+		elif i and i % 4 == 0:
+			print(' ', end='')
+		if b >= ord(' ') and b <= ord('~'):
+			a += chr(b)
+		else:
+			a += '.'
+		print('%02X' % b, end='')
+	if a:
+		rem = width - (i % width) - 1
+		print(' ' * (rem*2), end='')
+		print(' ' * (rem//4 + 1), end='')
+		print('| %s%s |' % (a, ' ' * rem))
+
+def parse_unknown_subshader_table(file):
+    hack_end = file.tell() + 0x9c0 # FIXME: Remove this
+    # At a guess, this might be the remaining info from the old .shader files,
+    # such as the OpenGL shader GPU IDs
+    print('  Subshader(?) table:')
+
+    (unknown_offset, num_subshader_tags) = struct.unpack('<2I', file.read(8))
+    ## This looks like an offset, since it gets progressively larger in each new
+    ## metadata section. The offset is too large to refer to this file as is
+    ## though - maybe post decompression?
+    print('    Unknown offset: 0x{0:08x} {0}'.format(unknown_offset))
+    print('    Num subshader(?) tags: %i' % num_subshader_tags)
+    if num_subshader_tags:
+        print('      Tags {', end='')
+        for i in range(num_subshader_tags):
+            print(' "%s"="%s"' % (parse_string(file), parse_string(file)), end='')
+        print(' }')
+
+    (u1, u2, u3, u4, u5) = struct.unpack('<5I', file.read(20))
+    assert(u1 == 0)
+    assert(u2 == 0)
+    assert(u3 == 6)
+    assert(u4 in (0x6, 0x12)) # Maybe an array length?
+    assert(u5 == 0)
+
+    # FIXME: Remove this
+    hack_data = file.read(hack_end - file.tell())
+    hexdump(hack_data, file.tell(), indent=2, width=24)
+
 def parse_unity55_shader(filename):
     file = open(filename, 'rb')
 
     (asset_name_len,) = struct.unpack('<I', file.read(4))
     assert(asset_name_len == 0)
 
-    parse_unknown_tex_table(file)
+    parse_properties_table(file)
 
+    # Maybe number of subshaders and/or passes?
     (u1, num_something) = struct.unpack('<2I', file.read(8))
     assert(u1 == 1)
 
@@ -101,9 +156,11 @@ def parse_unity55_shader(filename):
 
         parse_unknown_init_table(file)
 
+        parse_unknown_subshader_table(file)
+
         # TODO: Parse the next data structure, whatever it is
         if num_something > 1:
-            print(' WARNING FIXME: Skipping %i remaining shader metadata sections' % (num_something - 1))
+            print(' WARNING FIXME: Skipping %i remaining shader metadata sections' % (num_something - i))
             break
 
     print('TODO: Extract actual shaders...')
