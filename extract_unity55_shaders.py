b9b555c66b15613efe6ef764daa8e1813e9246fb
Ian Munsie <darkstarsword@gmail.com>
2017-12-29 02:18:23 +1100

    extract_unity55_shaders: Clean up parsing of format determined so far
    Doesn't parse anything new, but gives names to all identified fields and
    fixes some misinterpretations.

--- extract_unity55_shaders.py
+++ extract_unity55_shaders.py
@@ -4,6 +4,15 @@ import sys, os, argparse, glob, struct, zlib, itertools
 import extract_unity_shaders, extract_unity53_shaders
 from extract_unity53_shaders import align
 
+def align_verify_zero(file, alignment):
+    off = file.tell()
+    mod = off % alignment
+    if mod == 0:
+        return
+    data = file.read(mod)
+    print(data)
+    assert(data == b'\0' * mod)
+
 def parse_string(file):
     (length,) = struct.unpack('<I', file.read(4))
     string = file.read(length).decode('ascii')
@@ -19,69 +28,121 @@ def parse_properties_table(file):
     for i in range(table_len):
         name = parse_string(file)
         desc = parse_string(file)
-        (u1, u2, u3, r, g, b, a) = struct.unpack('<3I4f', file.read(7*4))
-        color = parse_string(file)
-        (u4,) = struct.unpack('<I', file.read(4))
+        (attributes, type, flags, def0, def1, def2, def3) = struct.unpack('<3I4f', file.read(7*4))
+        assert(attributes == 0) # FIXME: Attributes is probably an array, may be special cased for >= 0x10?
+        def_texture = parse_string(file)
+        (texdim,) = struct.unpack('<I', file.read(4))
         # FIXME: Match the formatting of the old .shader files
-        print('  {name} "{desc}" {u1:x} {u2:x} {u3:x} {r} {g} {b} {a} "{color}" {u4}'.format(
-            name=name, desc=desc, u1=u1, u2=u2, u3=u3, r=r, g=g, b=b, a=a, color=color, u4=u4))
+        print('  {name} "{desc}" {attributes:x} {type:x} {flags:x} {def0} {def1} {def2} {def3} "{def_texture}" {texdim}'.format(
+            name=name, desc=desc, attributes=attributes, type=type, flags=flags, def0=def0,
+            def1=def1, def2=def2, def3=def3, def_texture=def_texture, texdim=texdim))
     print('}')
 
-def parse_keywords_and_bindings_table(file):
+def parse_name_indices_table(file):
     (num_keywords_and_bindings,) = struct.unpack('<I', file.read(4))
     keywords_and_bindings_dict = {}
 
-    print('  Total keywords and bindings: %i' % num_keywords_and_bindings)
     for i in range(num_keywords_and_bindings):
-        keyword_or_binding_name = parse_string(file)
-        (keyword_idx,) = struct.unpack('<I', file.read(4))
-        assert(keyword_idx not in keywords_and_bindings_dict)
-        keywords_and_bindings_dict[keyword_idx] = keyword_or_binding_name
-        print('    Keyword/binding[%i]: %s' % (keyword_idx, keyword_or_binding_name))
+        name = parse_string(file)
+        (idx,) = struct.unpack('<I', file.read(4))
+        assert(idx not in keywords_and_bindings_dict)
+        keywords_and_bindings_dict[idx] = name
+        print('    Name[%i]: %s' % (idx, name))
 
-def check_init_indices(i, name):
-    # For now going on the assumption that the order of this table is constant,
-    # but not all entries are valid - if that is true, than we should see
-    # consistent names in the same position.
-    if name == '<noninit>': return
-    if i == 77: assert(name == 'unity_FogStart')
-    if i == 78: assert(name == 'unity_FogEnd')
-    if i == 79: assert(name == 'unity_FogDensity')
-    if i == 84: assert(name == 'unity_FogColor')
+def print_state_entry(name1, val, name2, indent=4):
+    print('{indent}{name1:20}: {val} {name2}'.format(name1=name1, val=val, name2=name2, indent = ' ' * indent))
+    #if name2 != '<noninit>':
+    #    assert(name1 == name2)
 
-def print_unknown_init_entry(file, i, pos, type, val, name):
-    print('    Init[{i} @ 0x{off:08x}]: {type}: {val} {name}'.format(
-        i=i, off=pos, type=type, val=val, name=name))
-    check_init_indices(i, name)
+def print_field(name, val, indent=4):
+    print('{indent}{name:20}: {val}'.format(name=name, val=val, indent = ' ' * indent))
 
-def parse_unknown_init_float_entry(file, i):
-    pos = file.tell()
+def parse_state_x4(file, verify_name, indent=4):
+    (val,) = struct.unpack('<I', file.read(4))
+    name = parse_string(file)
+    print_state_entry(verify_name, '0x%08x' % val, name, indent=indent)
+
+def parse_state_float(file, verify_name, indent=4):
     (val,) = struct.unpack('<f', file.read(4))
     name = parse_string(file)
-    print_unknown_init_entry(file, i, pos, 'float', '%.9g' % val, name)
+    print_state_entry(verify_name, '%.9g' % val, name, indent=indent)
 
-def parse_unknown_init_float2_entry(file, i):
-    pos = file.tell()
-    vals = struct.unpack('<2f', file.read(8))
-    name = parse_string(file)
-    print_unknown_init_entry(file, i, pos, 'float2', '%.9g %.9g' % vals, name)
+def parse_rt_blend_state(file, rt):
+    print('   RT Blend State[%i]:' % rt)
+    parse_state_float(file, 'srcBlend', indent=4)
+    parse_state_float(file, 'destBlend', indent=4)
+    parse_state_float(file, 'srcBlendAlpha', indent=4)
+    parse_state_float(file, 'destBlendAlpha', indent=4)
+    parse_state_float(file, 'blendOp', indent=4)
+    parse_state_float(file, 'blendOpAlpha', indent=4)
+    parse_state_float(file, 'colMask', indent=4)
 
-def parse_unknown_init_color_entry(file, i):
-    pos = file.tell()
-    name = parse_string(file)
+def parse_stencil_op_state(file, op):
+    print('   %s:' % op)
+    parse_state_float(file, 'pass', indent=4)
+    parse_state_float(file, 'fail', indent=4)
+    parse_state_float(file, 'zFail', indent=4)
+    parse_state_float(file, 'comp', indent=4)
+
+def parse_state_color(file, name1):
+    print('   %s:' % name1)
+    parse_state_float(file, 'x', indent=4)
+    parse_state_float(file, 'y', indent=4)
+    parse_state_float(file, 'z', indent=4)
+    parse_state_float(file, 'w', indent=4)
+    name2 = parse_string(file)
+    print('%s%s' % (' ' * 24, name2))
+
+def parse_x4(file, name, indent=4):
     (val,) = struct.unpack('<I', file.read(4))
-    print_unknown_init_entry(file, i, pos, 'color', '0x%08x' % val, name)
+    print_field(name, '0x%08x' % val, indent=indent)
 
-def parse_unknown_init_table(file):
-    print('  Unknown Initialisation Table:')
-    # No idea how to tell how large this table is, or which entries are what types
-    # Maybe it is hardcoded in Unity? I sense an amateur coder at work here.
-    for i in range(56):
-        parse_unknown_init_float_entry(file, i)
-    parse_unknown_init_float2_entry(file, 56)
-    for i in range(57, 84):
-        parse_unknown_init_float_entry(file, i)
-    parse_unknown_init_color_entry(file, 84)
+def parse_tags(file):
+    (num_pass_tags,) = struct.unpack('<I', file.read(4))
+    if num_pass_tags:
+        print('   Tags {', end='')
+        for i in range(num_pass_tags):
+            print(' "%s"="%s"' % (parse_string(file), parse_string(file)), end='')
+        print(' }')
+
+def parse_state(file):
+    (name_len,) = struct.unpack('<I', file.read(4))
+    assert(name_len == 0) # Guess this is a string, but make sure first
+
+    for rt in range(8):
+        parse_rt_blend_state(file, rt)
+
+    (rtSeparateBlend,) = struct.unpack('B', file.read(1))
+    print('   RT Separate Blend: %i' % rtSeparateBlend)
+    assert(file.read(3) == b'\0' * 3)
+
+    parse_state_float(file, 'zTest', indent=3)
+    parse_state_float(file, 'zWrite', indent=3)
+    parse_state_float(file, 'culling', indent=3)
+    parse_state_float(file, 'offsetFactor', indent=3)
+    parse_state_float(file, 'offsetUnits', indent=3)
+    parse_state_float(file, 'alphaToMask', indent=3)
+
+    parse_stencil_op_state(file, 'stencilOp')
+    parse_stencil_op_state(file, 'stencilOpFront')
+    parse_stencil_op_state(file, 'stencilOpBack')
+
+    parse_state_float(file, 'stencilSteadMask', indent=3)
+    parse_state_float(file, 'stencilWriteMask', indent=3)
+    parse_state_float(file, 'stencilRef', indent=3)
+    parse_state_float(file, 'fogStart', indent=3)
+    parse_state_float(file, 'fogEnd', indent=3)
+    parse_state_float(file, 'fogDensity', indent=3)
+
+    parse_state_color(file, 'fogColor')
+
+    parse_x4(file, 'fogMode', indent=3)
+    parse_x4(file, 'gpuProgramID', indent=3)
+
+    parse_tags(file)
+
+    parse_x4(file, 'LOD', indent=3)
+    parse_x4(file, 'lighting', indent=3)
 
 def hexdump(buf, start=0, width=16, indent=0):
 	a = ''
@@ -104,68 +165,27 @@ def hexdump(buf, start=0, width=16, indent=0):
 		print(' ' * (rem//4 + 1), end='')
 		print('| %s%s |' % (a, ' ' * rem))
 
-def parse_unknown_subshader_table(file):
-    hack_end = file.tell() + 0x9c0 # FIXME: Remove this
-    # At a guess, this might be the remaining info from the old .shader files,
-    # such as the OpenGL shader GPU IDs
-    print('  Subshader(?) table:')
+def parse_pass(file):
+    parse_name_indices_table(file)
 
-    (unknown_offset, num_subshader_tags) = struct.unpack('<2I', file.read(8))
-    ## This looks like an offset, since it gets progressively larger in each new
-    ## metadata section. The offset is too large to refer to this file as is
-    ## though - maybe post decompression?
-    print('    Unknown offset: 0x{0:08x} {0}'.format(unknown_offset))
-    print('    Num subshader(?) tags: %i' % num_subshader_tags)
-    if num_subshader_tags:
-        print('      Tags {', end='')
-        for i in range(num_subshader_tags):
-            print(' "%s"="%s"' % (parse_string(file), parse_string(file)), end='')
-        print(' }')
+    (type,) = struct.unpack('<I', file.read(4))
+    assert(type == 0)
 
-    (u1, u2, u3, unknown_table_len) = struct.unpack('<4I', file.read(16))
-    assert(u1 == 0)
-    assert(u2 == 0)
-    assert(u3 == 6)
-    print('    Unknown table 1 len: %u' % unknown_table_len)
+    parse_state(file)
 
-    for i in range(unknown_table_len):
-        print('      Unknown table 1 entry %i' % i)
-        data = file.read(136)
-        hexdump(data, file.tell(), indent=6, width=24)
+    parse_x4(file, 'ProgramMask', indent=3)
 
-    (unknown_table_len,) = struct.unpack('<I', file.read(4))
-    print('    Unknown table 2 len: %u' % unknown_table_len)
-
-    for i in range(unknown_table_len):
-        print('      Unknown table 2 entry %i' % i)
-        data = file.read(64)
-        hexdump(data, file.tell(), indent=6, width=24)
-
-    u4 = struct.unpack('<8I', file.read(32))
-    assert(u4 == (0,)*8)
-
-    (num_pass_tags,) = struct.unpack('<I', file.read(4))
-    if num_pass_tags:
-        print('      Tags {', end='')
-        for i in range(num_pass_tags):
-            print(' "%s"="%s"' % (parse_string(file), parse_string(file)), end='')
-        print(' }')
-
-    (u5,) = struct.unpack('<I', file.read(4))
-    assert(u5 == 0)
-
-    shader_name = parse_string(file)
-    print('Shader Name: %s' % shader_name)
-
-    u6 = struct.unpack('<4I', file.read(16))
-    assert(u6 == (0,)*4)
-
-    data = file.read(100)
-    hexdump(data, file.tell(), indent=4, width=24)
-
-    # FIXME: Remove this
-    hack_data = file.read(hack_end - file.tell())
-    hexdump(hack_data, file.tell(), indent=2, width=24)
+    # TODO: subprograms
+    # TODO:   progVertex
+    # TODO:   progFragment
+    # TODO:   progGeometry
+    # TODO:   progHull
+    # TODO:   progDomain
+    # TODO: hasInstancingVariant (1 byte)
+    # TODO: UseName = parse_string(file)
+    # TODO: Name = parse_string(file)
+    # TODO: TextureName = parse_string(file)
+    # TODO: parse_tags(file)
 
 def parse_unity55_shader(filename):
     file = open(filename, 'rb')
@@ -175,27 +195,33 @@ def parse_unity55_shader(filename):
 
     parse_properties_table(file)
 
-    # Maybe number of subshaders and/or passes?
-    (u1, num_something) = struct.unpack('<2I', file.read(8))
-    assert(u1 == 1)
-
-    print('Number of shader metadata sections: %i' % num_something)
-    for i in range(num_something):
-        print(' Shader metadata section %i:' % i)
-        parse_keywords_and_bindings_table(file)
-
-        (u3, u4) = struct.unpack('<2I', file.read(8))
-        assert(u3 == 0)
-        assert(u4 == 0)
-
-        parse_unknown_init_table(file)
-
-        parse_unknown_subshader_table(file)
-
-        # TODO: Parse the next data structure, whatever it is
-        if num_something > 1:
-            print(' WARNING FIXME: Skipping %i remaining shader metadata sections' % (num_something - i))
+    (num_subshaders,) = struct.unpack('<I', file.read(4))
+    print('Number of subshaders: %i' % num_subshaders)
+    for subshader in range(num_subshaders):
+        print(' Subshader %i:' % subshader)
+        (num_passes,) = struct.unpack('<I', file.read(4))
+        print('  Number of passes: %i' % num_passes)
+        for pass_no in range(num_passes):
+            print('  Pass %i:' % pass_no)
+            parse_pass(file)
+            if num_passes > 1:
+                print(' WARNING FIXME: Skipping %i remaining passes' % (num_passes - pass_no))
+                break
+        if num_subshaders > 1:
+            print(' WARNING FIXME: Skipping %i remaining subshaders' % (num_subshaders - subshader))
             break
+
+    # TODO: Name = parse_string(file)
+    # TODO: CustomEditorName = parse_string(file)
+    # TODO: FallbackName = parse_string(file)
+    # TODO: Dependencies
+    # TODO: DisableNoSubshadersMessage
+
+    # TODO: platforms
+    # TODO: offsets
+    # TODO: compressedLengths
+    # TODO: decompressedLengths
+    # TODO: compressedBlob
 
     print('TODO: Extract actual shaders...')
 
