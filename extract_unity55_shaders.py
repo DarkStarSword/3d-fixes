e4353450aca3f0ed93e08267766f72b0a315a25a
Ian Munsie <darkstarsword@gmail.com>
2017-12-29 11:37:31 +1100

    extract_unity55_shaders: Parse up to the end of the subshader metadata
    Huge progress reverse engineering this format - now decodes and dumps
    out all parameters up to the end of the subshader metadata, including
    bindings and so on that we will want later. Not much left to decipher -
    little more than the shaders and their names.
    
    Still have to convert this into a format that the following scripts are
    expecting, and obviously extract the shaders.

--- extract_unity55_shaders.py
+++ extract_unity55_shaders.py
@@ -2,16 +2,14 @@
 
 import sys, os, argparse, glob, struct, zlib, itertools
 import extract_unity_shaders, extract_unity53_shaders
-from extract_unity53_shaders import align
 
-def align_verify_zero(file, alignment):
+def align(file, alignment):
     off = file.tell()
     mod = off % alignment
     if mod == 0:
         return
-    data = file.read(mod)
-    print(data)
-    assert(data == b'\0' * mod)
+    data = file.read(alignment - mod)
+    assert(data == b'\0' * (alignment - mod))
 
 def parse_string(file):
     (length,) = struct.unpack('<I', file.read(4))
@@ -28,26 +26,33 @@ def parse_properties_table(file):
     for i in range(table_len):
         name = parse_string(file)
         desc = parse_string(file)
-        (attributes, type, flags, def0, def1, def2, def3) = struct.unpack('<3I4f', file.read(7*4))
-        assert(attributes == 0) # FIXME: Attributes is probably an array, may be special cased for >= 0x10?
+        (num_attributes,) = struct.unpack('<I', file.read(4))
+        attributes = []
+        for i in range(num_attributes):
+            attributes.append(parse_string(file))
+        (type, flags, def0, def1, def2, def3) = struct.unpack('<2I4f', file.read(6*4))
         def_texture = parse_string(file)
         (texdim,) = struct.unpack('<I', file.read(4))
         # FIXME: Match the formatting of the old .shader files
-        print('  {name} "{desc}" {attributes:x} {type:x} {flags:x} {def0} {def1} {def2} {def3} "{def_texture}" {texdim}'.format(
-            name=name, desc=desc, attributes=attributes, type=type, flags=flags, def0=def0,
+        print('  {name} "{desc}" {num_attributes:x} {type:x} {flags:x} {def0} {def1} {def2} {def3} "{def_texture}" {texdim}'.format(
+            name=name, desc=desc, num_attributes=num_attributes, type=type, flags=flags, def0=def0,
             def1=def1, def2=def2, def3=def3, def_texture=def_texture, texdim=texdim))
+        if attributes:
+            print('    Attributes: ' + ', '.join(attributes))
     print('}')
 
 def parse_name_indices_table(file):
-    (num_keywords_and_bindings,) = struct.unpack('<I', file.read(4))
-    keywords_and_bindings_dict = {}
+    (num_names,) = struct.unpack('<I', file.read(4))
+    name_dict = {}
 
-    for i in range(num_keywords_and_bindings):
+    for i in range(num_names):
         name = parse_string(file)
         (idx,) = struct.unpack('<I', file.read(4))
-        assert(idx not in keywords_and_bindings_dict)
-        keywords_and_bindings_dict[idx] = name
+        assert(idx not in name_dict)
+        name_dict[idx] = name
         print('    Name[%i]: %s' % (idx, name))
+
+    return name_dict
 
 def print_state_entry(name1, val, name2, indent=4):
     print('{indent}{name1:20}: {val} {name2}'.format(name1=name1, val=val, name2=name2, indent = ' ' * indent))
@@ -96,25 +101,37 @@ def parse_state_color(file, name1):
 def parse_x4(file, name, indent=4):
     (val,) = struct.unpack('<I', file.read(4))
     print_field(name, '0x%08x' % val, indent=indent)
+    return val
 
-def parse_tags(file):
+def parse_u4(file, name=None, indent=4):
+    (val,) = struct.unpack('<I', file.read(4))
+    if name is not None:
+        print_field(name, '%i' % val, indent=indent)
+    return val
+
+def parse_byte(file, name=None, indent=4):
+    (val,) = struct.unpack('B', file.read(1))
+    if name is not None:
+        print_field(name, '%i' % val, indent=indent)
+    return val
+
+def parse_tags(file, indent=3):
     (num_pass_tags,) = struct.unpack('<I', file.read(4))
     if num_pass_tags:
-        print('   Tags {', end='')
+        print('%sTags {' % (' ' * indent), end='')
         for i in range(num_pass_tags):
             print(' "%s"="%s"' % (parse_string(file), parse_string(file)), end='')
         print(' }')
 
 def parse_state(file):
-    (name_len,) = struct.unpack('<I', file.read(4))
-    assert(name_len == 0) # Guess this is a string, but make sure first
+    print('   State Name: %s' % parse_string(file))
 
     for rt in range(8):
         parse_rt_blend_state(file, rt)
 
     (rtSeparateBlend,) = struct.unpack('B', file.read(1))
     print('   RT Separate Blend: %i' % rtSeparateBlend)
-    assert(file.read(3) == b'\0' * 3)
+    align(file, 4)
 
     parse_state_float(file, 'zTest', indent=3)
     parse_state_float(file, 'zWrite', indent=3)
@@ -165,8 +182,99 @@ def hexdump(buf, start=0, width=16, indent=0):
 		print(' ' * (rem//4 + 1), end='')
 		print('| %s%s |' % (a, ' ' * rem))
 
+def parse_channels(file):
+    (num_bindings,) = struct.unpack('<I', file.read(4))
+    print('     Num Channel Bindings: %i' % num_bindings)
+
+    for i in range(num_bindings):
+        (source, dest) = struct.unpack('<2B', file.read(2))
+        print('      Channel binding %i -> %i' % (source, dest))
+    align(file, 4)
+
+    parse_x4(file, 'SourceMap', indent=5)
+
+def parse_keywords(file, name_dict):
+    (num_keywords,) = struct.unpack('<I', file.read(4))
+    for i in range(num_keywords):
+        (idx,) = struct.unpack('<H', file.read(2))
+        print('     Keyword: %s' % name_dict[idx])
+
+def parse_vector_params(file, name_dict, indent=5):
+    num = parse_u4(file, 'Num Vectors', indent=indent)
+    for i in range(num):
+        (NameIndex, Index, ArraySize, Type, Dim) = struct.unpack('<IIIBB', file.read(14))
+        print('%s Name: %s Index: %i ArraySize: %i Type: %i Dim: %i' %
+                (' ' * indent, name_dict[NameIndex], Index, ArraySize, Type, Dim))
+        align(file, 4)
+
+def parse_matrix_params(file, name_dict, indent=5):
+    num = parse_u4(file, 'Num Matrices', indent=indent)
+    for i in range(num):
+        (NameIndex, Index, ArraySize, Type, RowCount) = struct.unpack('<IIIBB', file.read(14))
+        print('%s Name: %s Index: %i ArraySize: %i Type: %i RowCount: %i' %
+                (' ' * indent, name_dict[NameIndex], Index, ArraySize, Type, RowCount))
+        align(file, 4)
+
+def parse_texture_params(file, name_dict, indent=5):
+    num = parse_u4(file, 'Num Textures', indent=indent)
+    for i in range(num):
+        (NameIndex, Index, SamplerIndex, Dim) = struct.unpack('<IIIB', file.read(13))
+        print('%s Name: %s Index: %i SamplerIndex: %i Dim: %i' %
+                (' ' * indent, name_dict[NameIndex], Index, SamplerIndex, Dim))
+        align(file, 4)
+
+def parse_buffer_params(file, name_dict, indent=5):
+    num = parse_u4(file, 'Num Buffers', indent=indent)
+    for i in range(num):
+        (NameIndex, Index) = struct.unpack('<II', file.read(8))
+        print('      %s: %i' % (name_dict[NameIndex], Index))
+
+def parse_cb_params(file, name_dict):
+    num = parse_u4(file, 'Num Constant Buffers', indent=5)
+    for i in range(num):
+        print('     Constant Buffer %i' % i)
+        NameIndex = parse_u4(file)
+        print('      Name: %s' % name_dict[NameIndex])
+        parse_matrix_params(file, name_dict, indent=6)
+        parse_vector_params(file, name_dict, indent=6)
+        Size = parse_u4(file, 'Size', indent=6)
+
+def parse_cb_bindings(file, name_dict):
+    num = parse_u4(file, 'Num Constant Buffer Bindings', indent=5)
+    for i in range(num):
+        (NameIndex, Index) = struct.unpack('<II', file.read(8))
+        print('      %s: %i' % (name_dict[NameIndex], Index))
+
+def parse_uav_params(file, name_dict):
+    num = parse_u4(file, 'Num UAVs', indent=5)
+    for i in range(num):
+        print('     UAV %i' % i)
+        (NameIndex, Index, OriginalIndex) = struct.unpack('<3I', file.read(12))
+        print('      %s: %i %i' % (name_dict[NameIndex], Index, OriginalIndex))
+
+def parse_subprograms(file, subprogram_type, name_dict):
+    print('   %s:' % subprogram_type)
+    num_subprograms = parse_u4(file, 'Num Subprograms')
+    for i in range(num_subprograms):
+        print('    Subprogram %i:' % i)
+        parse_x4(file, 'BlobIndex', indent=5)
+        parse_channels(file)
+        parse_keywords(file, name_dict)
+        (ShaderHardwareTier, GpuProgramType) = struct.unpack('<2B', file.read(2))
+        print('     ShaderHardwareTier: %i' % ShaderHardwareTier)
+        print('     GpuProgramType: %i' % GpuProgramType)
+        align(file, 4)
+
+        parse_vector_params(file, name_dict)
+        parse_matrix_params(file, name_dict)
+        parse_texture_params(file, name_dict)
+        parse_buffer_params(file, name_dict)
+        parse_cb_params(file, name_dict)
+        parse_cb_bindings(file, name_dict)
+        parse_uav_params(file, name_dict)
+
 def parse_pass(file):
-    parse_name_indices_table(file)
+    name_dict = parse_name_indices_table(file)
 
     (type,) = struct.unpack('<I', file.read(4))
     assert(type == 0)
@@ -175,17 +283,21 @@ def parse_pass(file):
 
     parse_x4(file, 'ProgramMask', indent=3)
 
-    # TODO: subprograms
-    # TODO:   progVertex
-    # TODO:   progFragment
-    # TODO:   progGeometry
-    # TODO:   progHull
-    # TODO:   progDomain
-    # TODO: hasInstancingVariant (1 byte)
-    # TODO: UseName = parse_string(file)
-    # TODO: Name = parse_string(file)
-    # TODO: TextureName = parse_string(file)
-    # TODO: parse_tags(file)
+    parse_subprograms(file, 'progVertex', name_dict)
+    parse_subprograms(file, 'progFragment', name_dict)
+    parse_subprograms(file, 'progGeometry', name_dict)
+    parse_subprograms(file, 'progHull', name_dict)
+    parse_subprograms(file, 'progDomain', name_dict)
+
+    parse_byte(file, 'hasInstancingVariant', indent=3)
+    align(file, 4)
+    UseName = parse_string(file)
+    Name = parse_string(file)
+    TextureName = parse_string(file)
+    print('   UseName: %s' % UseName)
+    print('   Name: %s' % Name)
+    print('   TextureName: %s' % TextureName)
+    parse_tags(file)
 
 def parse_unity55_shader(filename):
     file = open(filename, 'rb')
@@ -199,17 +311,15 @@ def parse_unity55_shader(filename):
     print('Number of subshaders: %i' % num_subshaders)
     for subshader in range(num_subshaders):
         print(' Subshader %i:' % subshader)
+
         (num_passes,) = struct.unpack('<I', file.read(4))
         print('  Number of passes: %i' % num_passes)
         for pass_no in range(num_passes):
             print('  Pass %i:' % pass_no)
             parse_pass(file)
-            if num_passes > 1:
-                print(' WARNING FIXME: Skipping %i remaining passes' % (num_passes - pass_no))
-                break
-        if num_subshaders > 1:
-            print(' WARNING FIXME: Skipping %i remaining subshaders' % (num_subshaders - subshader))
-            break
+
+        parse_tags(file, indent=2)
+        parse_u4(file, 'LOD', indent=2)
 
     # TODO: Name = parse_string(file)
     # TODO: CustomEditorName = parse_string(file)
