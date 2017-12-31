79c24d32a9e65a2f98b57212f8d78d2a8f52f278
Ian Munsie <darkstarsword@gmail.com>
2018-01-01 03:35:28 +1100

    unity_asset_bundle_extractor: Decompress blocks on demand
    This introduces two layers to massively speed up extracting shaders from
    asset bundles:
    
    First, it introduces a BlockStream class, which exposes a file like
    interface to access the decompressed data, but performs the
    decompression on the fly only for blocks that are actually read. This
    allows us to skip the vast majority of the time we would have to spend
    decompressing the entire contained asset files.
    
    Secondly, it introduces an OffsetIO class, which exposes a file like
    interface that wraps another file like interface such as a BlockStream,
    and applies an offset to the IO to allow us to map distinct files onto
    the block stream. This is essentially akin to an OS filesystem layer.

--- unity_asset_bundle_extractor.py
+++ unity_asset_bundle_extractor.py
@@ -4,6 +4,111 @@ import sys, os, struct, io
 import unity_asset_extractor
 from unity_asset_extractor import lz4_decompress
 
+class BlockStream(object):
+    def __init__(self, name, data_header, file):
+        (self.num_blocks,) = struct.unpack('>I', data_header.read(4))
+        self.blocks = []
+        self.blocks_start = file.tell()
+        self.name = name
+        self.file = file
+        self.cache = {}
+
+        print('Number of blocks: %i' % self.num_blocks)
+        for i in range(self.num_blocks):
+            (decompressed_size, compressed_size, flags) = struct.unpack('>IIH', data_header.read(10))
+            self.blocks.append((decompressed_size, compressed_size, flags))
+
+        self.seek(0)
+
+    def activate_block(self):
+        try:
+            self.block = self.cache[self.block_idx]
+        except KeyError:
+            print('BlockStream %s: Reading block %i/%i' % (self.name, self.block_idx + 1, self.num_blocks))
+        else:
+            # print('BlockStream %s: Using block cache %i/%i' % (self.name, self.block_idx + 1, self.num_blocks))
+            return
+
+        try:
+            (decompressed_size, compressed_size, flags) = self.blocks[self.block_idx]
+        except IndexError:
+            self.block_idx = None
+            self.block = io.BytesIO()
+            print('BlockStream %s: EOF' % self.name)
+            return
+
+        self.file.seek(self.blocks_start + self.compressed_pos)
+        self.block = io.BytesIO(self.file.read(compressed_size))
+
+        if flags & 0x3f == 0:
+            pass
+        elif flags & 0x3f == 0x3:
+            self.block = io.BytesIO(lz4_decompress(self.block, decompressed_size))
+        else:
+            assert(False) # FIXME: Unsupported compression scheme
+
+        self.cache[self.block_idx] = self.block
+
+    def next_block(self):
+        (decompressed_size, compressed_size, flags) = self.blocks[self.block_idx]
+        self.decompressed_pos += decompressed_size
+        self.compressed_pos += compressed_size
+        self.block_idx += 1
+        self.activate_block()
+
+    def read(self, size):
+        buf = self.block.read(size)
+        while len(buf) < size and self.block is not None:
+            self.next_block()
+            buf += self.block.read(size - len(buf))
+        return buf
+
+    def seek(self, pos, whence=0):
+        if whence == 0:
+            pass
+        elif whence == 1:
+            self.read(pos) # FIXME: Skip reading
+            return
+        else:
+            raise io.UnsupportedOperation()
+
+        self.decompressed_pos = 0
+        self.compressed_pos = 0
+        for self.block_idx, (decompressed_size, compressed_size, flags) in enumerate(self.blocks):
+            if self.decompressed_pos + decompressed_size > pos:
+                break
+            self.decompressed_pos += decompressed_size
+            self.compressed_pos += compressed_size
+
+        self.activate_block()
+        self.block.seek(pos - self.decompressed_pos)
+
+    def tell(self):
+        return self.decompressed_pos + self.block.tell()
+
+class OffsetIO(object):
+    def __init__(self, name, buf, offset, size):
+        self.name = name
+        self.buf = buf
+        self.offset = offset
+        self.size = size
+        self.seek(0)
+
+    def read(self, size=-1):
+        # FIXME: Prevent read past end of self.size
+        return self.buf.read(size)
+
+    def seek(self, target, whence=0):
+        if whence == 0:
+            return self.buf.seek(self.offset + target)
+        return self.buf.seek(target, whence)
+
+    def tell(self):
+        return self.buf.tell() - self.offset
+
+    def fileno(self):
+        raise io.UnsupportedOperation()
+
 def read_cstring(file):
     s = b''
     while True:
@@ -41,24 +146,7 @@ def analyse(file):
 
     assert(data_header.read(16) == b'\0' * 16)
 
-    decompressed = b''
-
-    (num_blocks,) = struct.unpack('>I', data_header.read(4))
-    print('Number of blocks: %i' % num_blocks)
-    for i in range(num_blocks):
-        (decompressed_size, compressed_size, flags) = struct.unpack('>IIH', data_header.read(10))
-        print('Decompressing block %i/%i' % (i+1, num_blocks))
-        # print('  decompressed size: %i' % decompressed_size)
-        # print('  compressed size: %i' % compressed_size)
-        # print('  flags: 0x%04x' % flags)
-        block = file.read(compressed_size)
-        if flags & 0x3f == 0x0:
-            pass
-        elif flags & 0x3f == 0x3:
-            block = lz4_decompress(io.BytesIO(block), decompressed_size)
-        else:
-            assert(False) # FIXME: Unsupported compression scheme
-        decompressed += block
+    block_stream = BlockStream(file.name, data_header, file)
 
     (num_files,) = struct.unpack('>I', data_header.read(4))
     print('Number of files: %i' % num_files)
@@ -66,9 +154,10 @@ def analyse(file):
         (offset, size, flags) = struct.unpack('>QQI', data_header.read(20))
         name = read_cstring(data_header)
         print(' File %i: 0x%016x %10u 0x%08x "%s"' % (i, offset, size, flags, name))
-        stream = io.BytesIO(decompressed[offset:offset+size])
-        stream.name = name
-        unity_asset_extractor.analyse(stream)
+
+        asset_stream = OffsetIO(name, block_stream, offset, size)
+
+        unity_asset_extractor.analyse(asset_stream)
 
     assert(data_header.read(1) == b'')
 
