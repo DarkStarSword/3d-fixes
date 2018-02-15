3718cf9b98ccb6a9ed46f632691db64fcabc4838
Ian Munsie <darkstarsword@gmail.com>
2018-02-15 13:55:04 +1100

    Subnautica: Fix HUD flicker
    Certain HUD elements, such as the popup box that appears while mousing
    over certain things flicker between screen depth and depth=1
    
    This game seems to project the HUD into the world, then project it back
    to the screen (probably because of VR?), which means that floating point
    error might put their depth at not quite exactly 1.0, so the driver
    heuristics get confused.
    
    This adds a tolerance around W ~= 1 to catch these and keep them at
    screen depth.
    
    Since this means adding the icon shader to the HUD list, the inventory
    icons are now adjusted, which we don't want. Add a
    allow_crosshair_adjust parameter so that shader can be excluded from
    crosshair style adjustments.

--- Subnautica/ShaderFixes/hud.hlsl
+++ Subnautica/ShaderFixes/hud.hlsl
@@ -1,25 +1,39 @@
 #define crosshair_visible IniParams[0].x
+#define hud_w1_tolerance IniParams[0].y
 #define cursor_showing IniParams[7].y
 #define texture_filter IniParams[1].y
+#define pda_preset IniParams[1].z
 
 #define crosshair_texture 2
 #define screen_depth_texture 3
 
 #include "crosshair.hlsl"
 
-void handle_hud(inout float4 pos)
+void handle_in_world_hud(inout float4 pos)
+{
+	float4 s = StereoParams.Load(0);
+
+	// Tablet, in-world WARNING display or some other in-world HUD
+	// Stupidly (and probably because of VR), this game projects a lot of
+	// HUD in world, then projects them back to screen space, so this
+	// matches more than you would expect. This affects the main menu and
+	// the popup box that appears when mousing over the icons at the bottom
+	// of the screen in the PDA, and the icons in the fabricator.
+
+	// Because of this double-projection the W==1 driver heuristic doesn't
+	// work due to floating point error, so we increase the tolerance
+	// ourselves to return things to screen depth.
+	if (pos.w != 1 && distance(pos.w, 1) < hud_w1_tolerance)
+		pos.x -= s.x * (pos.w - s.y);
+}
+
+void handle_hud(inout float4 pos, bool allow_crosshair_adjust = true)
 {
 	float4 s = StereoParams.Load(0);
 
 #ifdef UNITY_PER_DRAW
 	if (!all(UNITY_PER_DRAW.unity_ObjectToWorld._m30_m31_m32_m33 == float4(0, 0, 1, 1))) {
-		// Tablet or some other in-world HUD
-		// Gah, main menu also matches this, so the "Primary" &
-		// "Secondary" text in keyboard shortcuts remains adjusted (W
-		// ~= 1), while the rest of the menu is at screen depth (W ==
-		// 1). The menu actually appears to be positioned in-world then
-		// projected back to screen... probably for VR?
-		// nvm, it's minor
+		handle_in_world_hud(pos);
 		return;
 	}
 #endif
@@ -33,6 +47,11 @@ void handle_hud(inout float4 pos)
 	if (cursor_showing)
 		return;
 
+	// Blacklist inventory circles at bottom of screen, since they overlap
+	// with the mask. The actual icons are on a separate shader that does
+	// not allow_crosshair_adjust to keep them at screen depth - if we
+	// later need that shader auto-adjusted, we will need to change this to
+	// a Y > -0.84 threshold check instead.
 	if (texture_filter == screen_depth_texture)
 		return;
 
@@ -41,6 +60,7 @@ void handle_hud(inout float4 pos)
 		return;
 	}
 
-	// Otherwise, adjust to crosshair depth:
-	pos.x += adjust_from_depth_buffer(0, 0);
+	// Otherwise, adjust to crosshair depth if allowed for this shader:
+	if (allow_crosshair_adjust)
+		pos.x += adjust_from_depth_buffer(0, 0);
 }
