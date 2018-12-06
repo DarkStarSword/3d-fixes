27141798c785ea527a816b2b2218ff902249c6f4
Ian Munsie <darkstarsword@gmail.com>
2018-12-06 13:20:59 +1100

    unity_asset_extractor: Fix resource table alignment in v14 + v15 files
    DHR his this assertion in ReCore using the v15 file format, so apply the
    same fix as for the v17 file format to align the file to a multiple of 4
    bytes rather than assume a fixed three byte padding.
    
    Also apply the same fix to the v14 parser as I am 99% certain it will be
    the case there as well.

--- unity_asset_extractor.py
+++ unity_asset_extractor.py
@@ -410,11 +410,8 @@ def parse_version_14(file, version):
             u = struct.unpack('>4I', file.read(16))
             print(("   {:3}: {:3}" + " {:08x}" * 4).format(*([i, id] + list(u))))
 
-    (num_resources, u1, u2, u3) = struct.unpack('<I3B', file.read(7))
-    # FIXME: This is very likely supposed to be align(file, 4) like in v17 - needs testing
-    assert(u1 == 0)
-    assert(u2 == 0)
-    assert(u3 == 0)
+    (num_resources,) = struct.unpack('<I', file.read(4))
+    align(file, 4)
 
     print("Num resources: {}".format(num_resources))
     for i in range(num_resources):
@@ -464,11 +461,8 @@ def parse_version_15(file, version):
             u = struct.unpack('>4I', file.read(16))
             print(("   {:3}: {:3}" + " {:08x}" * 4).format(*([i, id] + list(u))))
 
-    (num_resources, u1, u2, u3) = struct.unpack('<I3B', file.read(7))
-    # FIXME: This is very likely supposed to be align(file, 4) like in v17 - needs testing
-    assert(u1 == 0)
-    assert(u2 == 0)
-    assert(u3 == 0)
+    (num_resources,) = struct.unpack('<I', file.read(4))
+    align(file, 4)
 
     print("Num resources: {}".format(num_resources))
     for i in range(num_resources):
