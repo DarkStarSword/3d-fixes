d6a49fd8dcfaaf41758340964258d38f689eac4f
Ian Munsie <darkstarsword@gmail.com>
2019-06-02 04:57:14 +1000

    Team Sonic Racing: Add a HUD adjustment while driving
    This pushes the HUD into depth while driving to make it easier to see
    team mate nameplates, incoming item alerts, etc.
    
    This game is problematic for doing sophisticated HUD adjustments for a
    number of reasons - the split screen and mirror mode make using a depth
    buffer adjustment tricky, since the most recent depth buffer may not be
    the one we need, and we would need to take into account mirror mode to
    flip the sample coordinates. The game also bunches numerous textures
    together and updates them live, making it impossible to identify
    specific textures from a command list or even to reliably detect any
    texture that might indicate we are racing... The final kick in the teeth
    is the mouse cursor that we would almost be able to use to determine
    when we are in a race... except half the time the game neglects to hide
    it at all.
    
    So, this ends up going for a different approach. The HUD adjustment is
    triggered by the accelerator key, since we can reasonably expect the
    player to hold that down most of the time they are racing, and we have a
    short release delay to allow for cornering, with transitions to avoid
    abrupt changes.
    
    The actual adjustment we use takes advantage of the fact that the race
    occurs on a road, so HUD elements near the bottom of the screen should
    be near the player car, and those higher in the screen should be further
    into depth. It interpolates between two depth values configurable in the
    d3dx.ini based on the screen Y value (which fortunately is relative to
    each individual split screen). The result is not perfect, but quite
    usable.

---
+++ Team Sonic Racing/ShaderFixes/hud.hlsl
@@ -0,0 +1,31 @@
+// We want to adjust the HUD so that nameplates, warning icons, etc line up
+// with the player / item they correspond with, but there are a few problems:
+//
+// 1. Split screen. The game draws the four worlds first (top left, top right,
+// bottom left, bottom right), then blits them to their final locations on the
+// screen and draws the HUD over the top. This means that the most recent depth
+// buffer, etc. won't correspond to the HUD being drawn, and we would have to
+// reliably track up to four depth buffers to consider using a crosshair.hlsl
+// style fix.
+//
+// 2. Mirror mode. If we use the depth buffer and mirror mode is enabled we
+// would need to mirror the X sample locations.
+//
+// For now we will do something simpler, that will hopefully work out
+// acceptably.
+
+#define hud_active IniParams[0].x
+#define min_hud IniParams[0].y
+#define max_hud IniParams[0].z
+#define hud_amount IniParams[0].w
+
+void adjust_hud(inout float4 pos)
+{
+	float4 s = StereoParams.Load(0);
+
+	if (pos.w == 1.0 && hud_active) {
+		float y = pos.y / 2 + 0.5;
+		float depth = lerp(min_hud, max_hud, y);
+		pos.x += s.x * (depth - s.y) / depth * hud_amount;
+	}
+}
